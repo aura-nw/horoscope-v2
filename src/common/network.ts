daeeeb016e8ed71ee00595e865c0ed874369cd32
phamphong9981 <phamphong9981@gmail.com>
2023-01-17 17:06:31 +0700

    feat: init network environment

--- src/common/network.ts
+++ src/common/network.ts
@@ -1,22 +1,43 @@
-const local = {
-  rpcEndpoint: 'http://localhost:26657',
-  prefix: 'aura',
-  denom: 'uaura',
-  chainId: 'local-aura',
-  broadcastTimeoutMs: 2000,
-  broadcastPollIntervalMs: 500,
-  // TODO: should calculate from onchain data
-  averageBlockTimeMs: 1000,
-};
+const network = {
+  development: {
+    rpcEndpoint: 'http://localhost:26657',
+    prefix: 'aura',
+    denom: 'uaura',
+    chainId: 'local-aura',
+    broadcastTimeoutMs: 2000,
+    broadcastPollIntervalMs: 500,
+    // TODO: should calculate from onchain data
+    averageBlockTimeMs: 1000,
+  },
+  test: {
+    rpcEndpoint: 'http://localhost:26667',
+    prefix: 'aura',
+    denom: 'uaura',
+    chainId: 'local-aura',
+    broadcastTimeoutMs: 2000,
+    broadcastPollIntervalMs: 500,
+    averageBlockTimeMs: 1000,
+  },
 
-const localTest = {
-  rpcEndpoint: 'http://localhost:26667',
-  prefix: 'aura',
-  denom: 'uaura',
-  chainId: 'local-aura',
-  broadcastTimeoutMs: 2000,
-  broadcastPollIntervalMs: 500,
-  averageBlockTimeMs: 1000,
+  // production: {
+  //   rpcEndpoint: 'http://localhost:26657',
+  //   prefix: 'aura',
+  //   denom: 'uaura',
+  //   chainId: 'local-aura',
+  //   broadcastTimeoutMs: 2000,
+  //   broadcastPollIntervalMs: 500,
+  //   // TODO: should calculate from onchain data
+  //   averageBlockTimeMs: 1000,
+  // },
+  // staging: {
+  //   rpcEndpoint: 'http://localhost:26657',
+  //   prefix: 'aura',
+  //   denom: 'uaura',
+  //   chainId: 'local-aura',
+  //   broadcastTimeoutMs: 2000,
+  //   broadcastPollIntervalMs: 500,
+  //   // TODO: should calculate from onchain data
+  //   averageBlockTimeMs: 1000,
+  // }
 };
-
-export default { local, localTest };
+export default network;
