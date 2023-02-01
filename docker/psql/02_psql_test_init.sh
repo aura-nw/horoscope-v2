45e9e460e3cddcbe7a6bf47bfe8464fef9f5830a
phamphong9981 <phamphong9981@gmail.com>
2023-02-01 14:44:02 +0700

    feat: hasura

--- docker/psql/02_psql_test_init.sh
+++ docker/psql/02_psql_test_init.sh
@@ -4,6 +4,8 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 	CREATE DATABASE "$POSTGRES_DB_TEST";
 	GRANT ALL PRIVILEGES ON DATABASE "$POSTGRES_DB_TEST" TO "$POSTGRES_USER";
+	CREATE DATABASE hasura;
+	GRANT ALL PRIVILEGES ON DATABASE hasura TO "$POSTGRES_USER";
 EOSQL
 
 psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB_TEST" -a -f ./docker-entrypoint-initdb.d/01_schema.sql
