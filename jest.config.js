4ab21b447470aaee40c96dc9fcebc6fd219500e6
doquockhanhan <doquockhanhan@gmail.com>
2023-03-09 08:44:29 +0700

    fix: edit file name in jest.config.ts

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,6 @@ export default {
   preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'node',
   runner: 'jest-light-runner',
-  globalSetup: './test/config/globalSetup.ts',
-  globalTeardown: './test/config/globalTeardown.ts',
+  globalSetup: './test/config/global-setup.ts',
+  globalTeardown: './test/config/global-teardown.ts',
 };
