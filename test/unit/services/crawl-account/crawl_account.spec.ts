00eec078903d15ad7964b06b2d09d9836d1cea04
doquockhanhan <doquockhanhan@gmail.com>
2023-04-05 11:10:15 +0700

    fix: remove check pubkey

--- test/unit/services/crawl-account/crawl_account.spec.ts
+++ test/unit/services/crawl-account/crawl_account.spec.ts
@@ -111,14 +111,6 @@ export default class CrawlAccountTest {
     expect(
       accounts.find(
         (acc) => acc.address === 'aura1qwexv7c6sm95lwhzn9027vyu2ccneaqa7c24zk'
-      )?.pubkey
-    ).toEqual({
-      '@type': '/cosmos.crypto.secp256k1.PubKey',
-      key: 'ApNMBAr8lFRS6DaOKXgGXFcrpf78KHyqPvRCLZrM0Zzg',
-    });
-    expect(
-      accounts.find(
-        (acc) => acc.address === 'aura1qwexv7c6sm95lwhzn9027vyu2ccneaqa7c24zk'
       )?.account_number
     ).toEqual(3);
   }
