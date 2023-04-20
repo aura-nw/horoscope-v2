3dfef6e8ffcf190621a43f302807504b104b1bfd
Phan Anh Tuan <fibonacci998@gmail.com>
2023-04-20 09:29:50 +0700

    fix: unit test function handle job, cannot test create job

--- test/unit/services/crawl-transaction/parse_transaction.spec.ts
+++ test/unit/services/crawl-transaction/parse_transaction.spec.ts
@@ -1,4 +1,4 @@
-import { AfterAll, BeforeAll, Describe, Test } from '@jest-decorated/core';
+import { BeforeAll, Describe, Test } from '@jest-decorated/core';
 import { ServiceBroker } from 'moleculer';
 import { Log } from '@cosmjs/stargate/build/logs';
 import { Attribute, Event } from '@cosmjs/stargate/build/events';
@@ -24,7 +24,7 @@ export default class CrawlTransactionTest {
     this.crawlTxService = this.broker.createService(
       CrawlTxService
     ) as CrawlTxService;
-    await Promise.all([
+    return Promise.all([
       this.crawlTxService
         ?.getQueueManager()
         .getQueue(BULL_JOB_NAME.CRAWL_TRANSACTION)
@@ -34,31 +34,25 @@ export default class CrawlTransactionTest {
         .getQueue(BULL_JOB_NAME.HANDLE_TRANSACTION)
         .empty(),
       knex.raw('TRUNCATE TABLE transaction RESTART IDENTITY CASCADE'),
-      Block.query().insert(
-        Block.fromJson({
-          height: 423136,
-          hash: 'data hash',
-          time: '2023-04-17T03:44:41.000Z',
-          proposer_address: 'proposer address',
-          data: {},
-        })
-      ),
     ]);
   }
 
   @Test('Parse transaction and insert to DB')
   public async testHandleTransaction() {
-    this.crawlTxService?.createJob(
-      BULL_JOB_NAME.HANDLE_TRANSACTION,
-      BULL_JOB_NAME.HANDLE_TRANSACTION,
-      {
-        listTx: { ...tx_fixture },
+    await Block.query().insert(
+      Block.fromJson({
         height: 423136,
-        timestamp: '2023-04-17T03:44:41.000Z',
-      }
+        hash: 'data hash',
+        time: '2023-04-17T03:44:41.000Z',
+        proposer_address: 'proposer address',
+        data: {},
+      })
     );
-    // eslint-disable-next-line no-promise-executor-return
-    await new Promise((r) => setTimeout(r, 2000));
+    await this.crawlTxService?.jobHandlerTx({
+      listTx: { ...tx_fixture },
+      height: 423136,
+      timestamp: '2023-04-17T03:44:41.000Z',
+    });
     const tx = await Transaction.query().findOne(
       'hash',
       '5F38B0C3E9FAB4423C37FB6306AC06D983AF50013BC7BCFBD9F684D6BFB0AF23'
@@ -80,16 +74,17 @@ export default class CrawlTransactionTest {
                 `${event.type}.${attribute.key}`
               )
               .andWhere('value', attribute.value);
-
             expect(found).not.toBeUndefined();
             expect(found.length).not.toEqual(0);
           });
         });
       });
     }
+    // }
+    // );
   }
 
-  @AfterAll()
+  @BeforeAll()
   async tearDown() {
     await Promise.all([
       this.crawlTxService
@@ -102,8 +97,9 @@ export default class CrawlTransactionTest {
         .empty(),
     ]);
     await Promise.all([
-      knex.raw('TRUNCATE TABLE transaction RESTART IDENTITY CASCADE'),
+      knex.raw('TRUNCATE TABLE block RESTART IDENTITY CASCADE'),
       this.crawlTxService?._stop(),
+      this.broker.stop(),
     ]);
   }
 }
