c6aa5e47002bc8f3becab18b92b71314e30c80dc
doquockhanhan <doquockhanhan@gmail.com>
2023-04-24 14:40:36 +0700

    feat: add function to terminate process after all genesis jobs are completed

--- src/services/crawl-genesis/crawl_genesis.service.ts
+++ src/services/crawl-genesis/crawl_genesis.service.ts
@@ -47,7 +47,11 @@ export default class CrawlGenesisService extends BullableService {
         .select('*')
         .findOne('job_name', BULL_JOB_NAME.CRAWL_GENESIS);
 
-    if (genesisBlkCheck && genesisBlkCheck.height > 0) return;
+    if (genesisBlkCheck && genesisBlkCheck.height > 0) {
+      this.logger.info('Genesis job had already been processed');
+      await this.terminateProcess();
+      return;
+    }
 
     if (!fs.existsSync('genesis.txt')) fs.appendFileSync('genesis.txt', '');
 
@@ -137,14 +141,27 @@ export default class CrawlGenesisService extends BullableService {
   })
   public async crawlGenesisAccounts(_payload: object): Promise<void> {
     this.logger.info('Crawl genesis accounts');
-    let genesis = JSON.parse(fs.readFileSync('genesis.txt').toString());
-    if (genesis.result) genesis = genesis.result.genesis;
 
-    const accountsInDb: Account[] = await Account.query();
+    const [accountsInDb, genesisCheckpoint]: [
+      Account[],
+      BlockCheckpoint | undefined
+    ] = await Promise.all([
+      Account.query(),
+      BlockCheckpoint.query()
+        .select('*')
+        .findOne('job_name', BULL_JOB_NAME.CRAWL_GENESIS),
+    ]);
     if (accountsInDb.length > 0) {
       this.logger.error('DB already contains some accounts');
       return;
     }
+    if (genesisCheckpoint?.height !== 1) {
+      this.logger.info('Job crawl genesis is still processing');
+      return;
+    }
+
+    let genesis = JSON.parse(fs.readFileSync('genesis.txt').toString());
+    if (genesis.result) genesis = genesis.result.genesis;
 
     const { balances } = genesis.app_state.bank;
     const auths: any = _.keyBy(
@@ -197,6 +214,19 @@ export default class CrawlGenesisService extends BullableService {
         })
       );
     }
+
+    await BlockCheckpoint.query()
+      .insert(
+        BlockCheckpoint.fromJson({
+          job_name: BULL_JOB_NAME.CRAWL_GENESIS_ACCOUNT,
+          height: 1,
+        })
+      )
+      .onConflict('job_name')
+      .merge()
+      .returning('id');
+
+    await this.terminateProcess();
   }
 
   @QueueHandler({
@@ -206,6 +236,15 @@ export default class CrawlGenesisService extends BullableService {
   })
   public async crawlGenesisValidators(_payload: object): Promise<void> {
     this.logger.info('Crawl genesis validators');
+
+    const genesisCheckpoint = await BlockCheckpoint.query()
+      .select('*')
+      .findOne('job_name', BULL_JOB_NAME.CRAWL_GENESIS);
+    if (genesisCheckpoint?.height !== 1) {
+      this.logger.info('Job crawl genesis is still processing');
+      return;
+    }
+
     let genesis = JSON.parse(fs.readFileSync('genesis.txt').toString());
     if (genesis.result) genesis = genesis.result.genesis;
 
@@ -241,6 +280,39 @@ export default class CrawlGenesisService extends BullableService {
       .onConflict('operator_address')
       .merge()
       .returning('id');
+
+    await BlockCheckpoint.query()
+      .insert(
+        BlockCheckpoint.fromJson({
+          job_name: BULL_JOB_NAME.CRAWL_GENESIS_VALIDATOR,
+          height: 1,
+        })
+      )
+      .onConflict('job_name')
+      .merge()
+      .returning('id');
+
+    await this.terminateProcess();
+  }
+
+  private async terminateProcess() {
+    const checkpoint = await BlockCheckpoint.query().whereIn('job_name', [
+      BULL_JOB_NAME.CRAWL_GENESIS_ACCOUNT,
+      BULL_JOB_NAME.CRAWL_GENESIS_VALIDATOR,
+    ]);
+
+    if (
+      checkpoint.find(
+        (check) => check.job_name === BULL_JOB_NAME.CRAWL_GENESIS_ACCOUNT
+      )?.height !== 1 ||
+      checkpoint.find(
+        (check) => check.job_name === BULL_JOB_NAME.CRAWL_GENESIS_VALIDATOR
+      )?.height !== 1
+    ) {
+      this.logger.info('Crawl genesis jobs are still processing');
+      return;
+    }
+    process.exit();
   }
 
   private async handleIbcDenom(accounts: Account[]): Promise<Account[]> {
