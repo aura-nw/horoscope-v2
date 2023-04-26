e49c24a1d0e93dd53e9609604ea912ea24943631
andqk <doquockhanhan@gmail.com>
2023-04-26 08:14:59 +0700

    Update crawl_proposal.service.ts

--- src/services/crawl-proposal/crawl_proposal.service.ts
+++ src/services/crawl-proposal/crawl_proposal.service.ts
@@ -63,6 +63,9 @@ export default class CrawlProposalService extends BullableService {
         .findOne('job_name', BULL_JOB_NAME.CRAWL_PROPOSAL),
       Block.query().select('height').findOne({}).orderBy('height', 'desc'),
     ]);
+    this.logger.info(
+      `Block Checkpoint: ${JSON.stringify(crawlProposalBlockCheckpoint)}`
+    );
 
     let lastHeight = 0;
     let updateBlockCheckpoint: BlockCheckpoint;
@@ -173,15 +176,16 @@ export default class CrawlProposalService extends BullableService {
           })
         );
 
-        await Proposal.query()
-          .insert(listProposals)
-          .onConflict('proposal_id')
-          .merge()
-          .returning('proposal_id')
-          .catch((error) => {
-            this.logger.error('Error insert or update proposals');
-            this.logger.error(error);
-          });
+        if (listProposals.length > 0)
+          await Proposal.query()
+            .insert(listProposals)
+            .onConflict('proposal_id')
+            .merge()
+            .returning('proposal_id')
+            .catch((error) => {
+              this.logger.error('Error insert or update proposals');
+              this.logger.error(error);
+            });
       }
 
       updateBlockCheckpoint.height = latestBlock.height;
@@ -239,11 +243,9 @@ export default class CrawlProposalService extends BullableService {
     const now = new Date(new Date().getSeconds() - 10);
 
     const depositProposals = await Proposal.query()
-      // .patch({
-      //   status: Proposal.STATUS.PROPOSAL_STATUS_NOT_ENOUGH_DEPOSIT,
-      // })
       .where('status', Proposal.STATUS.PROPOSAL_STATUS_DEPOSIT_PERIOD)
       .andWhere('deposit_end_time', '<=', now);
+    this.logger.info(`List not enough deposit proposals: ${JSON.stringify(depositProposals)}`);
 
     depositProposals.forEach((proposal: Proposal) => {
       const request: QueryProposalRequest = {
@@ -284,17 +286,18 @@ export default class CrawlProposalService extends BullableService {
         proposal.status = Proposal.STATUS.PROPOSAL_STATUS_NOT_ENOUGH_DEPOSIT;
     });
 
-    await Proposal.query()
-      .insert(depositProposals)
-      .onConflict('proposal_id')
-      .merge()
-      .returning('proposal_id')
-      .catch((error) => {
-        this.logger.error(
-          'Error update status for not enough deposit proposals'
-        );
-        this.logger.error(error);
-      });
+    if (depositProposals.length > 0)
+      await Proposal.query()
+        .insert(depositProposals)
+        .onConflict('proposal_id')
+        .merge()
+        .returning('proposal_id')
+        .catch((error) => {
+          this.logger.error(
+            'Error update status for not enough deposit proposals'
+          );
+          this.logger.error(error);
+        });
   }
 
   public async _start() {
