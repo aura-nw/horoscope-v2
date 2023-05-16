89958943cc24aa91835be36ce682e09be3ffd257
Phan Anh Tuan <fibonacci998@gmail.com>
2023-05-16 15:18:43 +0700

    feat: add decodeIbcHeader to readable

--- src/services/crawl-tx/aura.registry.ts
+++ src/services/crawl-tx/aura.registry.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import { Registry, GeneratedType } from '@cosmjs/proto-signing';
 import { defaultRegistryTypes as defaultStargateTypes } from '@cosmjs/stargate';
 
@@ -82,12 +83,173 @@ export default class AuraRegistry {
         }
       } else if (msg.typeUrl === MSG_TYPE.MSG_UPDATE_CLIENT) {
         if (result.header?.value && result.header?.typeUrl) {
-          result.header = this.registry.decode(result.header);
+          result.header = this.decodeIbcHeader(
+            this.registry.decode(result.header)
+          );
         }
       }
     }
 
     // eslint-disable-next-line consistent-return
     return result;
+  }
+
+  decodeIbcHeader(header: Header) {
+    let decodedIbcHeader: any = { ...header };
+    decodedIbcHeader = {
+      signedHeader: {
+        header: {
+          height: header.signedHeader?.header?.height.toString(),
+          version: {
+            app: header.signedHeader?.header?.version?.app.toString(),
+            block: header.signedHeader?.header?.version?.block.toString(),
+          },
+
+          appHash: header.signedHeader?.header?.appHash
+            ? toBase64(header.signedHeader?.header?.appHash)
+            : null,
+          dataHash: header.signedHeader?.header?.dataHash
+            ? toBase64(header.signedHeader?.header?.dataHash)
+            : null,
+          evidenceHash: header.signedHeader?.header?.evidenceHash
+            ? toBase64(header.signedHeader?.header?.evidenceHash)
+            : null,
+          lastBlockId: {
+            hash: header.signedHeader?.header?.lastBlockId?.hash
+              ? toBase64(header.signedHeader?.header?.lastBlockId?.hash)
+              : null,
+            partSetHeader: {
+              hash: header.signedHeader?.header?.lastBlockId?.partSetHeader
+                ?.hash
+                ? toBase64(
+                    header.signedHeader?.header?.lastBlockId?.partSetHeader
+                      ?.hash
+                  )
+                : null,
+              total:
+                header.signedHeader?.header?.lastBlockId?.partSetHeader?.total,
+            },
+          },
+          consensusHash: header.signedHeader?.header?.consensusHash
+            ? toBase64(header.signedHeader?.header?.consensusHash)
+            : null,
+          validatorsHash: header.signedHeader?.header?.validatorsHash
+            ? toBase64(header.signedHeader?.header?.validatorsHash)
+            : null,
+          lastCommitHash: header.signedHeader?.header?.lastCommitHash
+            ? toBase64(header.signedHeader?.header?.lastCommitHash)
+            : null,
+          proposerAddress: header.signedHeader?.header?.proposerAddress
+            ? toBase64(header.signedHeader?.header?.proposerAddress)
+            : null,
+          lastResultsHash: header.signedHeader?.header?.lastResultsHash
+            ? toBase64(header.signedHeader?.header?.lastResultsHash)
+            : null,
+          nextValidatorsHash: header.signedHeader?.header?.nextValidatorsHash
+            ? toBase64(header.signedHeader?.header?.nextValidatorsHash)
+            : null,
+        },
+        commit: {
+          round: header.signedHeader?.commit?.round,
+          height: header.signedHeader?.commit?.height.toString(),
+          blockId: {
+            hash: header.signedHeader?.commit?.blockId?.hash
+              ? toBase64(header.signedHeader?.commit?.blockId?.hash)
+              : null,
+            partSetHeader: {
+              hash: header.signedHeader?.commit?.blockId?.partSetHeader?.hash
+                ? toBase64(
+                    header.signedHeader?.commit?.blockId?.partSetHeader?.hash
+                  )
+                : null,
+              total: header.signedHeader?.commit?.blockId?.partSetHeader?.total,
+            },
+          },
+          signatures: decodedIbcHeader.signedHeader?.commit?.signatures.map(
+            (sig: any) => ({
+              ...sig,
+              signature: toBase64(sig.signature),
+              validatorAddress: toBase64(sig.validatorAddress),
+            })
+          ),
+        },
+      },
+      validatorSet: {
+        proposer: {
+          address: header.validatorSet?.proposer?.address
+            ? toBase64(header.validatorSet?.proposer?.address)
+            : null,
+          pubKey: {
+            ed25519: header.validatorSet?.proposer?.pubKey?.ed25519
+              ? toBase64(header.validatorSet?.proposer?.pubKey?.ed25519)
+              : null,
+            secp256k1: header.validatorSet?.proposer?.pubKey?.secp256k1
+              ? toBase64(header.validatorSet?.proposer?.pubKey?.secp256k1)
+              : null,
+          },
+          votingPower: header.validatorSet?.proposer?.votingPower.toString(),
+          proposerPriority:
+            header.validatorSet?.proposer?.proposerPriority.toString(),
+        },
+        validators: decodedIbcHeader.validatorSet?.validators.map(
+          (val: any) => ({
+            ...val,
+            address: val.address ? toBase64(val.address) : null,
+            pubKey: {
+              ed25519: val.pubKey?.ed25519
+                ? toBase64(val.pubKey?.ed25519)
+                : null,
+              secp256k1: val.pubKey?.secp256k1
+                ? toBase64(val.pubKey?.secp256k1)
+                : null,
+            },
+            votingPower: val.votingPower.toString(),
+            proposerPriority: val.proposerPriority.toString(),
+          })
+        ),
+        totalVotingPower: header.validatorSet?.totalVotingPower.toString(),
+      },
+      trustedHeight: {
+        revisionNumber: header.trustedHeight?.revisionNumber.toString(),
+        revisionHeight: header.trustedHeight?.revisionHeight.toString(),
+      },
+      trustedValidators: {
+        proposer: {
+          address: header.trustedValidators?.proposer?.address
+            ? toBase64(header.trustedValidators?.proposer?.address)
+            : null,
+          pubKey: {
+            ed25519: header.trustedValidators?.proposer?.pubKey?.ed25519
+              ? toBase64(header.trustedValidators?.proposer?.pubKey?.ed25519)
+              : null,
+            secp256k1: header.trustedValidators?.proposer?.pubKey?.secp256k1
+              ? toBase64(header.trustedValidators?.proposer?.pubKey?.secp256k1)
+              : null,
+          },
+          votingPower:
+            header.trustedValidators?.proposer?.votingPower.toString(),
+          proposerPriority:
+            header.trustedValidators?.proposer?.proposerPriority.toString(),
+        },
+        validators: decodedIbcHeader.trustedValidators?.validators.map(
+          (val: any) => ({
+            ...val,
+            address: val.address ? toBase64(val.address) : null,
+            pubKey: {
+              ed25519: val.pubKey?.ed25519
+                ? toBase64(val.pubKey?.ed25519)
+                : null,
+              secp256k1: val.pubKey?.secp256k1
+                ? toBase64(val.pubKey?.secp256k1)
+                : null,
+            },
+            votingPower: val.votingPower.toString(),
+            proposerPriority: val.proposerPriority.toString(),
+          })
+        ),
+        totalVotingPower: header.trustedValidators?.totalVotingPower.toString(),
+      },
+    };
+    return decodedIbcHeader;
   }
 }
