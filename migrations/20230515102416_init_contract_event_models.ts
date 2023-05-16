0fdf01abe9e178027c7f8813d998037cba45a24f
phamphong9981 <phamphong9981@gmail.com>
2023-05-16 14:29:01 +0700

    feat: init models

--- migrations/20230515102416_init_contract_event_models.ts
+++ migrations/20230515102416_init_contract_event_models.ts
@@ -1,29 +1,31 @@
 import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.createTable('contract_event', (table) => {
+  await knex.schema.createTable('smart_contract_event', (table) => {
     table.increments('id').primary();
     table.integer('smart_contract_id').unique().index().notNullable();
     table.string('action');
     table.integer('event_id').index().notNullable();
-    table.integer('index');
+    table.integer('index').notNullable();
     table.foreign('smart_contract_id').references('smart_contract.id');
     table.foreign('event_id').references('event.id');
     table.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'));
     table.timestamp('updated_at').notNullable().defaultTo(knex.raw('now()'));
   });
-  await knex.schema.createTable('contract_event_attribute', (table) => {
+  await knex.schema.createTable('smart_contract_event_attribute', (table) => {
     table.increments('id').primary();
-    table.integer('contract_event_id').unique().index().notNullable();
+    table.integer('smart_contract_event_id').unique().index().notNullable();
     table.string('key');
     table.string('value');
-    table.foreign('contract_event_id').references('contract_event.id');
+    table
+      .foreign('smart_contract_event_id')
+      .references('smart_contract_event.id');
     table.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'));
     table.timestamp('updated_at').notNullable().defaultTo(knex.raw('now()'));
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTable('contract_event_attribute');
-  await knex.schema.dropTable('contract_event');
+  await knex.schema.dropTable('smart_contract_event');
+  await knex.schema.dropTable('smart_contract_event_attribute');
 }
