09b84161a281ce1af7b478c5ca87dbcc66094861
phamphong9981 <phamphong9981@gmail.com>
2023-05-16 12:15:11 +0700

    refactor: code

--- src/common/utils/s3.ts
+++ src/common/utils/s3.ts
@@ -1,29 +1,21 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 // eslint-disable-next-line import/no-extraneous-dependencies
 import AWS from 'aws-sdk';
-import config from '../../../config.json' assert { type: 'json' };
+import { Config } from '..';
 
+const { AWS_SECRET_ACCESS_KEY, AWS_ACCESS_KEY_ID, AWS_REGION } = Config;
 export class S3Service {
-  private AWS_ACCESS_KEY_ID: string;
-
-  private AWS_SECRET_ACCESS_KEY: string;
-
-  private AWS_REGION: string;
-
-  private S3_CLIENT: any;
+  private s3Client: AWS.S3;
 
   public constructor() {
-    this.AWS_ACCESS_KEY_ID = config.s3.awsAccessKeyId;
-    this.AWS_SECRET_ACCESS_KEY = config.s3.awsSecretAccessKey;
-    this.AWS_REGION = config.s3.awsRegion;
-    this.S3_CLIENT = new AWS.S3({
-      accessKeyId: this.AWS_ACCESS_KEY_ID,
-      region: this.AWS_REGION,
-      secretAccessKey: this.AWS_SECRET_ACCESS_KEY,
+    this.s3Client = new AWS.S3({
+      accessKeyId: AWS_ACCESS_KEY_ID,
+      region: AWS_REGION,
+      secretAccessKey: AWS_SECRET_ACCESS_KEY,
     });
   }
 
   public connectS3() {
-    return this.S3_CLIENT;
+    return this.s3Client;
   }
 }
