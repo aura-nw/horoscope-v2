fc4a204e8cf998e356e995eb952940c7207ac32b
doquockhanhan <doquockhanhan@gmail.com>
2023-05-24 17:29:10 +0700

    feat: add handle voting end proposals

--- test/unit/services/crawl-proposal/crawl_proposal.spec.ts
+++ test/unit/services/crawl-proposal/crawl_proposal.spec.ts
@@ -320,7 +320,7 @@ export default class CrawlProposalTest {
   public async testHandleNotEnoughDepositProposal() {
     await Proposal.query()
       .patch({
-        deposit_end_time: new Date(new Date().getSeconds() - 10).toISOString(),
+        deposit_end_time: new Date(Date.now() - 10).toISOString(),
         status: Proposal.STATUS.PROPOSAL_STATUS_DEPOSIT_PERIOD,
       })
       .where({ proposal_id: 1 });
