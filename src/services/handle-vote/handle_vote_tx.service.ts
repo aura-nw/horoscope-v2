d49b24eae852d3e9d99152cdeb4ca2b86530f39e
Phan Anh Tuan <fibonacci998@gmail.com>
2023-05-25 10:49:43 +0700

    fix: remove not used code

--- src/services/handle-vote/handle_vote_tx.service.ts
+++ src/services/handle-vote/handle_vote_tx.service.ts
@@ -1,8 +1,5 @@
 import { Service } from '@ourparentcenter/moleculer-decorators-extended';
 import { ServiceBroker } from 'moleculer';
-import { SigningStargateClient } from '@cosmjs/stargate';
-import { DirectSecp256k1HdWallet, coins } from '@cosmjs/proto-signing';
-import { cosmos } from '@aura-nw/aurajs';
 import { BlockCheckpoint, TransactionMessage, Vote } from '../../models';
 import { BULL_JOB_NAME, MSG_TYPE, SERVICE } from '../../common/constant';
 import BullableService, { QueueHandler } from '../../base/bullable.service';
@@ -90,49 +87,7 @@ export default class HandleTxVoteService extends BullableService {
     });
   }
 
-  async testVote() {
-    const account1 = {
-      address: 'aura17dv9s7hujzkruzmezeg4l39zfuks7mjfscddcm',
-      mnemonic:
-        'sting erupt teach roof dentist eagle found garment spatial panther slice hold despair feed service avocado enemy pigeon tone craft off radar special purity',
-    };
-    const client1 = await SigningStargateClient.connectWithSigner(
-      'https://rpc.dev.aura.network/',
-      await DirectSecp256k1HdWallet.fromMnemonic(account1.mnemonic, {
-        prefix: 'aura',
-      })
-    );
-    const result = await client1.signAndBroadcast(
-      account1.address,
-      [
-        {
-          typeUrl: '/cosmos.gov.v1beta1.MsgVote',
-          value: cosmos.gov.v1beta1.MsgVote.fromPartial({
-            option: cosmos.gov.v1beta1.VoteOption.VOTE_OPTION_NO_WITH_VETO,
-            proposalId: 428,
-            voter: account1.address,
-          }),
-        },
-        {
-          typeUrl: '/cosmos.gov.v1beta1.MsgVote',
-          value: cosmos.gov.v1beta1.MsgVote.fromPartial({
-            option: cosmos.gov.v1beta1.VoteOption.VOTE_OPTION_NO,
-            proposalId: 428,
-            voter: account1.address,
-          }),
-        },
-      ],
-      {
-        amount: coins(25000, 'utaura'),
-        gas: '1500000',
-      },
-      'test grant vote'
-    );
-    this.logger.info(result);
-  }
-
   public async _start(): Promise<void> {
-    // this.testVote();
     this.createJob(
       BULL_JOB_NAME.HANDLE_VOTE_TX,
       BULL_JOB_NAME.HANDLE_VOTE_TX,
