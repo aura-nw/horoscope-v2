f1f41458aaa90734f6999ee6902546ddc31fd508
doquockhanhan <doquockhanhan@gmail.com>
2023-05-29 10:40:37 +0700

    feat: add both end checkpoint (handle_tx and crawl_validator)

--- src/services/crawl-validator/handle_stake_event.service.ts
+++ src/services/crawl-validator/handle_stake_event.service.ts
@@ -40,7 +40,7 @@ export default class HandleStakeEventService extends BullableService {
     const [startHeight, endHeight, updateBlockCheckpoint] =
       await BlockCheckpoint.getCheckpoint(
         BULL_JOB_NAME.HANDLE_STAKE_EVENT,
-        [BULL_JOB_NAME.CRAWL_VALIDATOR],
+        [BULL_JOB_NAME.HANDLE_TRANSACTION, BULL_JOB_NAME.CRAWL_VALIDATOR],
         config.handleStakeEvent.key
       );
     this.logger.info(`startHeight: ${startHeight}, endHeight: ${endHeight}`);
