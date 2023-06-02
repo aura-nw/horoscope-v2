bb4b3303fae82b0b16e7cfadf12346c2cf80b18e
phamphong9981 <phamphong9981@gmail.com>
2023-06-02 13:59:17 +0700

    feat: create view holder

---
+++ migrations/20230602032214_create_view_count_cw721.ts
@@ -0,0 +1,26 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createView('view_count_holder_cw721', (viewBuilder) => {
+    viewBuilder.as(
+      knex('cw721_token')
+        .innerJoin(
+          'cw721_contract',
+          'cw721_token.cw721_contract_id',
+          'cw721_contract.id'
+        )
+        .innerJoin(
+          'smart_contract',
+          'cw721_contract.contract_id',
+          'smart_contract.id'
+        )
+        .select(['cw721_token.owner', 'smart_contract.address'])
+        .count()
+        .groupBy(['cw721_token.owner', 'smart_contract.address'])
+    );
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropView('view_count_holder_cw721');
+}
