c12314f346c3cbea134f9804af6c95f5601803ff
AnDQK <doquockhanhan@gmail.com>
2023-06-08 09:36:39 +0700

    feat: remove comments and ip

--- src/common/types/errors.ts
+++ src/common/types/errors.ts
@@ -4,12 +4,6 @@ export enum ErrorMessage {
   SUCCESSFUL = 'Successful',
   NOT_FOUND = 'Not Found',
   WRONG = 'Wrong',
-  // NOT_ACTIVE = 'notactive',
-  // DUPLICATED_LOGIN = 'user.duplicated.login',
-  // DUPLICATED_EMAIL = 'user.duplicated.email',
-  // DELETE_ITSELF = 'user.delete.itself',
-  // ADDRESS_NOT_FOUND = 'Address not found',
-  // CRAWL_SUCCESSFUL = 'Data is preparing! Please call API again in a few minutes',
   VALIDATION_ERROR = 'Validation Error',
   BAD_REQUEST = 'Bad Request',
 }
