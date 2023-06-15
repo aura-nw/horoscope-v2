835ac34ede15cbaa4b45e9a02162cda4bb5a4d79
phamphong9981 <phamphong9981@gmail.com>
2023-06-15 16:11:01 +0700

    feat: track

--- src/services/cw20/cw20.service.ts
+++ src/services/cw20/cw20.service.ts
@@ -130,6 +130,7 @@ export default class Cw20Service extends BullableService {
                   (BigInt(acc) + BigInt(curr.amount)).toString(),
                 '0'
               ),
+              track: true,
             }),
             holders: initBalances.map((e) => ({
               address: e.address,
@@ -157,33 +158,37 @@ export default class Cw20Service extends BullableService {
         .whereIn(
           'smart_contract.address',
           cw20Events.map((event) => event.contract_address)
-        ),
+        )
+        .andWhere('track', true),
       (e) => `${e.smart_contract.address}`
     );
     // insert new histories
     await Cw20Event.query()
       .insert(
-        cw20Events.map((event) =>
-          Cw20Event.fromJson({
-            smart_contract_event_id: event.id,
-            sender: event.sender,
-            action: event.action,
-            cw20_contract_id: cw20ContractsByAddress[event.contract_address].id,
-            amount: getAttributeFrom(
-              event.attributes,
-              EventAttribute.ATTRIBUTE_KEY.AMOUNT
-            ),
-            from: getAttributeFrom(
-              event.attributes,
-              EventAttribute.ATTRIBUTE_KEY.FROM
-            ),
-            to: getAttributeFrom(
-              event.attributes,
-              EventAttribute.ATTRIBUTE_KEY.TO
-            ),
-            height: event.height,
-          })
-        )
+        cw20Events
+          .filter((event) => cw20ContractsByAddress[event.contract_address].id)
+          .map((event) =>
+            Cw20Event.fromJson({
+              smart_contract_event_id: event.id,
+              sender: event.sender,
+              action: event.action,
+              cw20_contract_id:
+                cw20ContractsByAddress[event.contract_address].id,
+              amount: getAttributeFrom(
+                event.attributes,
+                EventAttribute.ATTRIBUTE_KEY.AMOUNT
+              ),
+              from: getAttributeFrom(
+                event.attributes,
+                EventAttribute.ATTRIBUTE_KEY.FROM
+              ),
+              to: getAttributeFrom(
+                event.attributes,
+                EventAttribute.ATTRIBUTE_KEY.TO
+              ),
+              height: event.height,
+            })
+          )
       )
       .transacting(trx);
   }
