4c886582f32ffe22ee30ced4f76878ec2739b7e8
phamphong9981 <phamphong9981@gmail.com>
2023-06-22 15:34:33 +0700

    feat: logger update by contract

--- src/services/cw20/cw20_update_by_contract.service.ts
+++ src/services/cw20/cw20_update_by_contract.service.ts
@@ -41,6 +41,7 @@ export default class Cw20UpdateByContractService extends BullableService {
       .andWhere('height', '>', startBlock)
       .andWhere('height', '<=', endBlock);
     if (newEvents.length > 0) {
+      this.logger.info(newEvents);
       await knex.transaction(async (trx) => {
         const addAmount = await this.updateBalanceHolders(
           newEvents,
