bb10c8fadafaabb1aabd60acb922a2618db59144
phamphong9981 <phamphong9981@gmail.com>
2023-07-04 10:32:31 +0700

    refactor: code

--- src/services/cw721/cw721-missing-contract.service.ts
+++ src/services/cw721/cw721-missing-contract.service.ts
@@ -7,12 +7,11 @@ import { Context, ServiceBroker } from 'moleculer';
 import config from '../../../config.json' assert { type: 'json' };
 import BullableService from '../../base/bullable.service';
 import {
-  BULL_JOB_NAME,
   IContextCrawlMissingContractHistory,
   SERVICE,
   getHttpBatchClient,
 } from '../../common';
-import { BlockCheckpoint, SmartContract } from '../../models';
+import { SmartContract } from '../../models';
 import CW721Contract from '../../models/cw721_contract';
 import CW721Token from '../../models/cw721_token';
 
@@ -44,12 +43,6 @@ export default class Cw721MissingContractService extends BullableService {
       .where('address', ctx.params.contractAddress)
       .first()
       .throwIfNotFound();
-    const cw721BlockCheckpoint = (
-      await BlockCheckpoint.query()
-        .select('*')
-        .findOne('job_name', BULL_JOB_NAME.HANDLE_CW721_TRANSACTION)
-        .throwIfNotFound()
-    ).height;
     // check whether contract is CW721 type -> throw error to user
     if (smartContract.code.type === 'CW721') {
       const cw721Contract = await CW721Contract.query()
@@ -80,49 +73,45 @@ export default class Cw721MissingContractService extends BullableService {
               )
             )
           : 0;
-      if (maxUpdatedHeightOwner - cw721BlockCheckpoint < 2000000) {
-        CW721Token.softDelete = false;
-        await CW721Contract.query().upsertGraph({
-          ...CW721Contract.fromJson({
-            id: cw721Contract?.id,
-            contract_id: smartContract.id,
-            symbol: contractInfo?.symbol,
-            minter: contractInfo?.minter,
-            name: contractInfo?.name,
-            track: true,
-          }),
-          tokens: currentTokensOwner.map((tokenOwner) =>
-            CW721Token.fromJson({
-              token_id: tokenOwner.token_id,
-              media_info: null,
-              owner: tokenOwner.owner,
-              last_updated_height: tokenOwner.last_updated_height,
-              burned: false,
-            })
-          ),
-        });
-        CW721Token.softDelete = true;
-        // handle from minUpdatedHeightOwner to blockHeight
-        await this.broker.call(
-          SERVICE.V1.Cw721.HandleRangeBlockMissingContract.path,
-          {
-            smartContractId: smartContract.id,
-            startBlock: minUpdatedHeightOwner,
-            endBlock: maxUpdatedHeightOwner,
-          } satisfies IContextCrawlMissingContractHistory
-        );
-        // insert histories
-        await this.broker.call(
-          SERVICE.V1.Cw721.CrawlMissingContractHistory.path,
-          {
-            smartContractId: smartContract.id,
-            startBlock: config.crawlBlock.startBlock,
-            endBlock: maxUpdatedHeightOwner,
-          } satisfies IContextCrawlMissingContractHistory
-        );
-      } else {
-        throw new Error('CW721 service sync too slowly');
-      }
+      CW721Token.softDelete = false;
+      await CW721Contract.query().upsertGraph({
+        ...CW721Contract.fromJson({
+          id: cw721Contract?.id,
+          contract_id: smartContract.id,
+          symbol: contractInfo?.symbol,
+          minter: contractInfo?.minter,
+          name: contractInfo?.name,
+          track: true,
+        }),
+        tokens: currentTokensOwner.map((tokenOwner) =>
+          CW721Token.fromJson({
+            token_id: tokenOwner.token_id,
+            media_info: null,
+            owner: tokenOwner.owner,
+            last_updated_height: tokenOwner.last_updated_height,
+            burned: false,
+          })
+        ),
+      });
+      CW721Token.softDelete = true;
+      // handle from minUpdatedHeightOwner to blockHeight
+      await this.broker.call(
+        SERVICE.V1.Cw721.HandleRangeBlockMissingContract.path,
+        {
+          smartContractId: smartContract.id,
+          startBlock: minUpdatedHeightOwner,
+          endBlock: maxUpdatedHeightOwner,
+        } satisfies IContextCrawlMissingContractHistory
+      );
+      // insert histories
+      await this.broker.call(
+        SERVICE.V1.Cw721.CrawlMissingContractHistory.path,
+        {
+          smartContractId: smartContract.id,
+          startBlock: config.crawlBlock.startBlock,
+          endBlock: maxUpdatedHeightOwner,
+        } satisfies IContextCrawlMissingContractHistory
+      );
     } else {
       throw new Error(
         `Smart contract ${ctx.params.contractAddress} is not CW721 type`
