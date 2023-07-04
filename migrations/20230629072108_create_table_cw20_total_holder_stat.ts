82538d43748b3f736f6be1efc748f284ea14f1d6
phamphong9981 <phamphong9981@gmail.com>
2023-07-04 13:51:32 +0700

    refactor: index

--- migrations/20230629072108_create_table_cw20_total_holder_stat.ts
+++ migrations/20230629072108_create_table_cw20_total_holder_stat.ts
@@ -3,10 +3,10 @@ import { Knex } from 'knex';
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('cw20_total_holder_stats', (table) => {
     table.increments();
-    table.integer('cw20_contract_id').notNullable();
+    table.integer('cw20_contract_id').notNullable().index();
     table.foreign('cw20_contract_id').references('cw20_contract.id');
     table.integer('total_holder');
-    table.date('created_at').notNullable().defaultTo(knex.raw('now()'));
+    table.date('created_at').index().notNullable().defaultTo(knex.raw('now()'));
     table.unique(['cw20_contract_id', 'created_at']);
   });
 }
