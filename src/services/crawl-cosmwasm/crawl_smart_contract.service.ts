ec7bfb84421af3431f5693c8ee675f69d837f996
andqk <doquockhanhan@gmail.com>
2023-07-10 23:35:05 +0700

    feat: set job handle contract migration as interval

--- src/services/crawl-cosmwasm/crawl_smart_contract.service.ts
+++ src/services/crawl-cosmwasm/crawl_smart_contract.service.ts
@@ -379,6 +379,20 @@ export default class CrawlSmartContractService extends BullableService {
         },
       }
     );
+    this.createJob(
+      BULL_JOB_NAME.HANDLE_MIGRATE_CONTRACT,
+      BULL_JOB_NAME.HANDLE_MIGRATE_CONTRACT,
+      {},
+      {
+        removeOnComplete: true,
+        removeOnFail: {
+          count: 3,
+        },
+        repeat: {
+          every: config.crawlSmartContract.millisecondCrawl,
+        },
+      }
+    );
 
     return super._start();
   }
