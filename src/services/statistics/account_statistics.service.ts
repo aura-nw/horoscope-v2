194b3729f77638bffa3849ebe835d5ac33bb6d0f
AnDQK <doquockhanhan@gmail.com>
2023-07-26 14:15:35 +0700

    fix: query event from db and group it based on event_id and tx_id

--- src/services/statistics/account_statistics.service.ts
+++ src/services/statistics/account_statistics.service.ts
@@ -20,13 +20,15 @@ import {
 } from '../../common';
 import config from '../../../config.json' assert { type: 'json' };
 import BullableService, { QueueHandler } from '../../base/bullable.service';
-import Utils from '../../common/utils/utils';
+import knex from '../../common/utils/db_connection';
 
 @Service({
   name: SERVICE.V1.AccountStatisticsService.key,
   version: 1,
 })
 export default class AccountStatisticsService extends BullableService {
+  private accountStats: any;
+
   public constructor(public broker: ServiceBroker) {
     super(broker);
   }
@@ -64,226 +66,87 @@ export default class AccountStatisticsService extends BullableService {
     // prefix: `horoscope-v2-${config.chainId}`,
   })
   public async handleJob(_payload: IAccountStatsParam): Promise<void> {
-    try {
-      const { accountStats, date } = _payload;
+    const { date } = _payload;
+    this.accountStats = _payload.accountStats;
 
-      const startTime = dayjs
-        .utc(date)
-        .subtract(1, 'day')
-        .startOf('day')
-        .toDate();
-      const endTime = dayjs.utc(date).startOf('day').toDate();
+    const endTime = dayjs.utc(date).startOf('day').toDate();
+    const startTime = dayjs.utc(endTime).subtract(1, 'day').toDate();
 
-      let startTxId = _payload.startId;
+    let startTxId = _payload.startId;
+    // The end tx's ID needs to plus 1 since the query will select record with _lt not _lte
+    let nextId = Math.min(_payload.endId + 1, startTxId + 50);
+    let endTxId = _payload.endId;
+    // If job runs for the 1st time, then needs to query the start and end tx id of the day as the range
+    if (!_payload.startId && !_payload.endId) {
+      const startTx = await Transaction.query()
+        .select('id')
+        .where('transaction.timestamp', '>=', startTime)
+        .limit(1)
+        .orderBy('id');
+      const endTx = await Transaction.query()
+        .select('id')
+        .where('transaction.timestamp', '<', endTime)
+        .limit(1)
+        .orderBy('id', 'desc');
+
+      startTxId = startTx[0].id;
       // The end tx's ID needs to plus 1 since the query will select record with _lt not _lte
-      let nextId = Math.min(_payload.endId + 1, startTxId + 50);
-      let endTxId = _payload.endId;
-      // If job runs for the 1st time, then needs to query the start and end tx id of the day as the range
-      if (!_payload.startId && !_payload.endId) {
-        const startTx = await Transaction.query()
-          .select('id')
-          .where('transaction.timestamp', '>=', startTime)
-          .limit(1)
-          .orderBy('id');
-        const endTx = await Transaction.query()
-          .select('id')
-          .where('transaction.timestamp', '<', endTime)
-          .limit(1)
-          .orderBy('id', 'desc');
+      nextId = Math.min(endTx[0].id + 1, startTxId + 50);
+      endTxId = endTx[0].id;
+    }
+    this.logger.info(
+      `Get account statistic events from id ${startTxId} for day ${new Date(
+        startTime
+      )}`
+    );
 
-        startTxId = startTx[0].id;
-        // The end tx's ID needs to plus 1 since the query will select record with _lt not _lte
-        nextId = Math.min(endTx[0].id + 1, startTxId + 50);
-        endTxId = endTx[0].id;
-      }
-      this.logger.info(
-        `Get account statistic events from id ${startTxId} for day ${new Date(
-          startTime
-        )}`
+    const [spendReceiveDone, gasTxSentDone] = await Promise.all([
+      this.calculateSpendReceive(startTxId, nextId),
+      this.calculateGasUsedTxSent(startTxId, nextId),
+    ]);
+
+    if (!spendReceiveDone || !gasTxSentDone) {
+      await this.createJob(
+        BULL_JOB_NAME.CRAWL_ACCOUNT_STATISTICS,
+        BULL_JOB_NAME.CRAWL_ACCOUNT_STATISTICS,
+        {
+          startId: nextId,
+          endId: endTxId,
+          date: _payload.date,
+          accountStats: this.accountStats,
+        },
+        {
+          removeOnComplete: true,
+          removeOnFail: {
+            count: 3,
+          },
+        }
+      );
+    } else {
+      const dailyAccountStats = Object.keys(this.accountStats).map((acc) =>
+        AccountStatistics.fromJson({
+          address: acc,
+          ...this.accountStats[acc],
+          date: startTime.toISOString(),
+        })
       );
 
-      const [dailyEvents, feeEvents]: [EventAttribute[], any[]] =
-        await Promise.all([
-          EventAttribute.query()
-            .select('event_id', 'composite_key', 'value')
-            .where('tx_id', '>=', startTxId)
-            .andWhere('tx_id', '<', nextId)
-            .andWhere((builder) =>
-              builder
-                // Get the address that actually spent or received token
-                .whereIn('composite_key', [
-                  EventAttribute.ATTRIBUTE_COMPOSITE_KEY.COIN_SPENT_SPENDER,
-                  EventAttribute.ATTRIBUTE_COMPOSITE_KEY.COIN_SPENT_AMOUNT,
-                  EventAttribute.ATTRIBUTE_COMPOSITE_KEY.COIN_RECEIVED_AMOUNT,
-                  EventAttribute.ATTRIBUTE_COMPOSITE_KEY.COIN_RECEIVED_RECEIVER,
-                ])
-            ),
-          EventAttribute.query()
-            .joinRelated('transaction')
-            .select(
-              'event_attribute.tx_id',
-              'event_attribute.composite_key',
-              'event_attribute.value',
-              'transaction.gas_used'
-            )
-            .where('tx_id', '>=', startTxId)
-            .andWhere('tx_id', '<', nextId)
-            .andWhere((builder) =>
-              builder
-                // If fee_grant is involved, then needs to track to the granters and grantees
-                .whereIn('composite_key', [
-                  EventAttribute.ATTRIBUTE_COMPOSITE_KEY.TX_FEE_PAYER,
-                  EventAttribute.ATTRIBUTE_COMPOSITE_KEY.USE_FEEGRANT_GRANTEE,
-                ])
-            ),
-        ]);
-
-      if (dailyEvents.length > 0 || feeEvents.length > 0) {
-        dailyEvents
-          .concat(feeEvents)
-          .filter(
-            (event) =>
-              Utils.isValidAccountAddress(
-                event.value,
-                config.networkPrefixAddress,
-                20
-              ) ||
-              Utils.isValidAccountAddress(
-                event.value,
-                config.networkPrefixAddress,
-                32
-              )
-          )
-          .forEach((event) => {
-            if (!accountStats[event.value]) {
-              accountStats[event.value] = {
-                amount_sent: '0',
-                amount_received: '0',
-                tx_sent: 0,
-                gas_used: '0',
-              };
-            }
-          });
-
-        dailyEvents
-          .filter(
-            (event) =>
-              event.composite_key ===
-                EventAttribute.ATTRIBUTE_COMPOSITE_KEY.COIN_SPENT_AMOUNT ||
-              event.composite_key ===
-                EventAttribute.ATTRIBUTE_COMPOSITE_KEY.COIN_RECEIVED_AMOUNT
-          )
-          .forEach((event) => {
-            switch (event.composite_key) {
-              case EventAttribute.ATTRIBUTE_COMPOSITE_KEY.COIN_SPENT_AMOUNT:
-                const addrSpent = dailyEvents.find(
-                  (dEvent) =>
-                    dEvent.event_id === event.event_id &&
-                    dEvent.composite_key ===
-                      EventAttribute.ATTRIBUTE_COMPOSITE_KEY.COIN_SPENT_SPENDER
-                )!.value;
-
-                accountStats[addrSpent].amount_sent = (
-                  BigInt(accountStats[addrSpent].amount_sent) +
-                  BigInt(parseCoins(event.value)[0].amount)
-                ).toString();
-                break;
-              case EventAttribute.ATTRIBUTE_COMPOSITE_KEY.COIN_RECEIVED_AMOUNT:
-                const addrReceived = dailyEvents.find(
-                  (dEvent) =>
-                    dEvent.event_id === event.event_id &&
-                    dEvent.composite_key ===
-                      EventAttribute.ATTRIBUTE_COMPOSITE_KEY
-                        .COIN_RECEIVED_RECEIVER
-                )!.value;
-
-                accountStats[addrReceived].amount_received = (
-                  BigInt(accountStats[addrReceived].amount_received) +
-                  BigInt(parseCoins(event.value)[0].amount)
-                ).toString();
-                break;
-              default:
-                break;
-            }
-          });
-
-        feeEvents
-          .filter(
-            (event) =>
-              event.composite_key ===
-              EventAttribute.ATTRIBUTE_COMPOSITE_KEY.TX_FEE_PAYER
-          )
-          .forEach((event) => {
-            let addr = event.value;
-
-            const feeGrant = feeEvents.find(
-              (dEvent) =>
-                dEvent.tx_id === event.tx_id &&
-                dEvent.composite_key ===
-                  EventAttribute.ATTRIBUTE_COMPOSITE_KEY.USE_FEEGRANT_GRANTEE
-            );
-
-            if (feeGrant) {
-              addr = feeGrant.value;
-            }
-
-            accountStats[addr].tx_sent += 1;
-            accountStats[addr].gas_used = (
-              BigInt(accountStats[addr].gas_used) + BigInt(event.gas_used)
-            ).toString();
-          });
-
-        await this.createJob(
-          BULL_JOB_NAME.CRAWL_ACCOUNT_STATISTICS,
-          BULL_JOB_NAME.CRAWL_ACCOUNT_STATISTICS,
-          {
-            startId: nextId,
-            endId: endTxId,
-            date: _payload.date,
-            accountStats,
-          },
-          {
-            removeOnComplete: true,
-            removeOnFail: {
-              count: 3,
-            },
-          }
-        );
-      } else {
-        const dailyAccountStats = Object.keys(accountStats).map((acc) =>
-          AccountStatistics.fromJson({
-            address: acc,
-            ...accountStats[acc],
-            date: startTime.toISOString(),
-          })
-        );
-
-        this.logger.info(`Insert new daily statistic for date ${startTime}`);
-        if (dailyAccountStats.length > 0) {
-          await AccountStatistics.query()
-            .insert(dailyAccountStats)
-            .catch((error) => {
-              this.logger.error(
-                'Error insert new daily account statistic records'
-              );
-              this.logger.error(error);
-            });
-        }
-
-        await this.createJob(
-          BULL_JOB_NAME.HANDLE_TOP_ACCOUNTS,
-          BULL_JOB_NAME.HANDLE_TOP_ACCOUNTS,
-          {},
-          {
-            removeOnComplete: true,
-            removeOnFail: {
-              count: 3,
-            },
-          }
-        );
+      this.logger.info(`Insert new account statistics for date ${startTime}`);
+      if (dailyAccountStats.length > 0) {
+        await AccountStatistics.query().insert(dailyAccountStats);
       }
-    } catch (error) {
-      this.logger.error(error);
+
+      await this.createJob(
+        BULL_JOB_NAME.HANDLE_TOP_ACCOUNTS,
+        BULL_JOB_NAME.HANDLE_TOP_ACCOUNTS,
+        {},
+        {
+          removeOnComplete: true,
+          removeOnFail: {
+            count: 3,
+          },
+        }
+      );
     }
   }
 
@@ -293,27 +156,171 @@ export default class AccountStatisticsService extends BullableService {
     // prefix: `horoscope-v2-${config.chainId}`,
   })
   public async handleTopAccounts(_payload: object): Promise<void> {
-    try {
-      const now = dayjs.utc().startOf('day').toDate();
+    const now = dayjs.utc().startOf('day').toDate();
 
-      const { dayRange } = config.accountStatistics;
-      const [threeDayStat, fifteenDayStat, thirtyDayStat] = await Promise.all([
-        this.getStatsFromSpecificDaysAgo(dayRange[0], now),
-        this.getStatsFromSpecificDaysAgo(dayRange[1], now),
-        this.getStatsFromSpecificDaysAgo(dayRange[2], now),
-      ]);
+    const { dayRange } = config.accountStatistics;
+    const [threeDayStat, fifteenDayStat, thirtyDayStat] = await Promise.all([
+      this.getStatsFromSpecificDaysAgo(dayRange[0], now),
+      this.getStatsFromSpecificDaysAgo(dayRange[1], now),
+      this.getStatsFromSpecificDaysAgo(dayRange[2], now),
+    ]);
 
-      const topAccounts = {
-        three_days: this.calculateTop(threeDayStat),
-        fifteen_days: this.calculateTop(fifteenDayStat),
-        thirty_days: this.calculateTop(thirtyDayStat),
-      };
+    const topAccounts = {
+      three_days: this.calculateTop(threeDayStat),
+      fifteen_days: this.calculateTop(fifteenDayStat),
+      thirty_days: this.calculateTop(thirtyDayStat),
+    };
 
-      this.logger.info(`Update top accounts for day ${new Date(now)}`);
-      await this.broker.cacher?.set(REDIS_KEY.TOP_ACCOUNTS, topAccounts);
-    } catch (error) {
-      this.logger.error(error);
+    this.logger.info(`Update top accounts for day ${new Date(now)}`);
+    await this.broker.cacher?.set(REDIS_KEY.TOP_ACCOUNTS, topAccounts);
+  }
+
+  private async calculateSpendReceive(
+    startTxId: number,
+    nextId: number
+  ): Promise<boolean> {
+    const dailyEvents: any[] = await EventAttribute.query()
+      .select(knex.raw('jsonb_agg(jsonb_build_object(composite_key, value))'))
+      .where('tx_id', '>=', startTxId)
+      .andWhere('tx_id', '<', nextId)
+      .andWhere((builder) =>
+        builder
+          // Get the address that actually spent or received token
+          .whereIn('composite_key', [
+            EventAttribute.ATTRIBUTE_COMPOSITE_KEY.COIN_SPENT_SPENDER,
+            EventAttribute.ATTRIBUTE_COMPOSITE_KEY.COIN_SPENT_AMOUNT,
+            EventAttribute.ATTRIBUTE_COMPOSITE_KEY.COIN_RECEIVED_AMOUNT,
+            EventAttribute.ATTRIBUTE_COMPOSITE_KEY.COIN_RECEIVED_RECEIVER,
+          ])
+      )
+      .groupBy('event_id');
+
+    if (dailyEvents.length > 0) {
+      dailyEvents
+        .map((event) => Object.assign({}, ...event.jsonb_agg))
+        .map(
+          (event) =>
+            event[EventAttribute.ATTRIBUTE_COMPOSITE_KEY.COIN_SPENT_SPENDER] ??
+            event[EventAttribute.ATTRIBUTE_COMPOSITE_KEY.COIN_RECEIVED_RECEIVER]
+        )
+        .forEach((address) => {
+          if (!this.accountStats[address]) {
+            this.accountStats[address] = {
+              amount_sent: '0',
+              amount_received: '0',
+              tx_sent: 0,
+              gas_used: '0',
+            };
+          }
+        });
+
+      dailyEvents
+        .map((event) => Object.assign({}, ...event.jsonb_agg))
+        .forEach((event) => {
+          if (
+            event[EventAttribute.ATTRIBUTE_COMPOSITE_KEY.COIN_SPENT_SPENDER]
+          ) {
+            const addrSpent =
+              event[EventAttribute.ATTRIBUTE_COMPOSITE_KEY.COIN_SPENT_SPENDER];
+            const amountSpent =
+              event[EventAttribute.ATTRIBUTE_COMPOSITE_KEY.COIN_SPENT_AMOUNT];
+
+            this.accountStats[addrSpent].amount_sent = (
+              BigInt(this.accountStats[addrSpent].amount_sent) +
+              BigInt(parseCoins(amountSpent)[0].amount)
+            ).toString();
+          } else if (
+            event[EventAttribute.ATTRIBUTE_COMPOSITE_KEY.COIN_RECEIVED_RECEIVER]
+          ) {
+            const addrReceived =
+              event[
+                EventAttribute.ATTRIBUTE_COMPOSITE_KEY.COIN_RECEIVED_RECEIVER
+              ];
+            const amountReceived =
+              event[
+                EventAttribute.ATTRIBUTE_COMPOSITE_KEY.COIN_RECEIVED_AMOUNT
+              ];
+
+            this.accountStats[addrReceived].amount_received = (
+              BigInt(this.accountStats[addrReceived].amount_received) +
+              BigInt(parseCoins(amountReceived)[0].amount)
+            ).toString();
+          }
+        });
+
+      return false;
     }
+
+    return true;
+  }
+
+  private async calculateGasUsedTxSent(
+    startTxId: number,
+    nextId: number
+  ): Promise<boolean> {
+    const feeEvents: any[] = await EventAttribute.query()
+      .joinRelated('transaction')
+      .select(
+        knex.raw('jsonb_agg(jsonb_build_object(composite_key, value))'),
+        'gas_used'
+      )
+      .where('tx_id', '>=', startTxId)
+      .andWhere('tx_id', '<', nextId)
+      .andWhere((builder) =>
+        builder
+          // If fee_grant is involved, then needs to track to the granters and grantees
+          .whereIn('composite_key', [
+            EventAttribute.ATTRIBUTE_COMPOSITE_KEY.TX_FEE_PAYER,
+            EventAttribute.ATTRIBUTE_COMPOSITE_KEY.USE_FEEGRANT_GRANTEE,
+          ])
+      )
+      .groupBy('tx_id', 'gas_used');
+
+    if (feeEvents.length > 0) {
+      feeEvents
+        .map((event) => Object.assign({}, ...event.jsonb_agg))
+        .map(
+          (event) =>
+            event[EventAttribute.ATTRIBUTE_COMPOSITE_KEY.TX_FEE_PAYER] ??
+            event[EventAttribute.ATTRIBUTE_COMPOSITE_KEY.USE_FEEGRANT_GRANTEE]
+        )
+        .forEach((address) => {
+          if (!this.accountStats[address]) {
+            this.accountStats[address] = {
+              amount_sent: '0',
+              amount_received: '0',
+              tx_sent: 0,
+              gas_used: '0',
+            };
+          }
+        });
+
+      feeEvents
+        .map((event) =>
+          Object.assign({ gas_used: event.gas_used }, ...event.jsonb_agg)
+        )
+        .forEach((event) => {
+          let addr = event[EventAttribute.ATTRIBUTE_COMPOSITE_KEY.TX_FEE_PAYER];
+
+          if (
+            event[EventAttribute.ATTRIBUTE_COMPOSITE_KEY.USE_FEEGRANT_GRANTEE]
+          ) {
+            addr =
+              event[
+                EventAttribute.ATTRIBUTE_COMPOSITE_KEY.USE_FEEGRANT_GRANTEE
+              ];
+          }
+
+          this.accountStats[addr].tx_sent += 1;
+          this.accountStats[addr].gas_used = (
+            BigInt(this.accountStats[addr].gas_used) + BigInt(event.gas_used)
+          ).toString();
+        });
+
+      return false;
+    }
+
+    return true;
   }
 
   private async getStatsFromSpecificDaysAgo(
--- src/services/statistics/daily_statistics.service.ts
+++ src/services/statistics/daily_statistics.service.ts
@@ -64,112 +64,105 @@ export default class DailyStatisticsService extends BullableService {
     // prefix: `horoscope-v2-${config.chainId}`,
   })
   public async handleJob(_payload: IDailyStatsParam): Promise<void> {
-    try {
-      const startTime = dayjs
-        .utc(_payload.date)
-        .subtract(1, 'day')
-        .startOf('day')
-        .toDate();
-      const endTime = dayjs.utc(_payload.date).startOf('day').toDate();
-      this.logger.info(
-        `Get daily statistic events at page ${
-          _payload.offset
-        } for day ${new Date(startTime)}`
-      );
+    const endTime = dayjs.utc(_payload.date).startOf('day').toDate();
+    const startTime = dayjs.utc(endTime).subtract(1, 'day').toDate();
+    this.logger.info(
+      `Get daily statistic events at page ${_payload.offset} for day ${new Date(
+        startTime
+      )}`
+    );
 
-      let startTxId = _payload.startId;
-      let endTxId = _payload.endId;
-      // If job runs for the 1st time, then needs to query the start and end tx id of the day as the range
-      if (!_payload.startId && !_payload.endId) {
-        const startTx = await Transaction.query()
-          .select('id')
-          .where('transaction.timestamp', '>=', startTime)
-          .limit(1)
-          .orderBy('id');
-        const endTx = await Transaction.query()
-          .select('id')
-          .where('transaction.timestamp', '<', endTime)
-          .limit(1)
-          .orderBy('id', 'desc');
+    let startTxId = _payload.startId;
+    let endTxId = _payload.endId;
+    // If job runs for the 1st time, then needs to query the start and end tx id of the day as the range
+    if (!_payload.startId && !_payload.endId) {
+      const startTx = await Transaction.query()
+        .select('id')
+        .where('transaction.timestamp', '>=', startTime)
+        .limit(1)
+        .orderBy('id');
+      const endTx = await Transaction.query()
+        .select('id')
+        .where('transaction.timestamp', '<', endTime)
+        .limit(1)
+        .orderBy('id', 'desc');
 
-        startTxId = startTx[0].id;
-        endTxId = endTx[0].id;
-      }
+      startTxId = startTx[0].id;
+      endTxId = endTx[0].id;
+    }
 
-      const dailyAddresses = await EventAttribute.query()
-        .distinct('value')
-        .where('tx_id', '>=', startTxId)
-        .andWhere('tx_id', '<=', endTxId)
-        .andWhere('value', 'like', `${config.networkPrefixAddress}%`)
-        .limit(config.dailyStatistics.recordsPerCall)
-        .offset(_payload.offset * config.dailyStatistics.recordsPerCall);
+    const dailyAddresses = await EventAttribute.query()
+      .distinct('value')
+      .where('tx_id', '>=', startTxId)
+      .andWhere('tx_id', '<=', endTxId)
+      .andWhere('value', 'like', `${config.networkPrefixAddress}%`)
+      .limit(config.dailyStatistics.recordsPerCall)
+      .offset(_payload.offset * config.dailyStatistics.recordsPerCall);
 
-      if (dailyAddresses.length > 0) {
-        const activeAddrs = Array.from(
-          new Set(
-            _payload.addresses.concat(
-              dailyAddresses
-                .filter((event) =>
-                  Utils.isValidAccountAddress(
-                    event.value,
-                    config.networkPrefixAddress,
-                    20
-                  )
+    // TODO: Need to re-define if it just count normal addresses only or also the contract addresses
+    if (dailyAddresses.length > 0) {
+      const activeAddrs = Array.from(
+        new Set(
+          _payload.addresses.concat(
+            dailyAddresses
+              .filter((event) =>
+                Utils.isValidAccountAddress(
+                  event.value,
+                  config.networkPrefixAddress,
+                  20
                 )
-                .map((event) => event.value)
-            )
+              )
+              .map((event) => event.value)
           )
-        );
-        const offset = _payload.offset + 1;
+        )
+      );
+      const offset = _payload.offset + 1;
 
-        await this.createJob(
-          BULL_JOB_NAME.CRAWL_DAILY_STATISTICS,
-          BULL_JOB_NAME.CRAWL_DAILY_STATISTICS,
-          {
-            startId: startTxId,
-            endId: endTxId,
-            date: _payload.date,
-            offset,
-            addresses: activeAddrs,
+      await this.createJob(
+        BULL_JOB_NAME.CRAWL_DAILY_STATISTICS,
+        BULL_JOB_NAME.CRAWL_DAILY_STATISTICS,
+        {
+          startId: startTxId,
+          endId: endTxId,
+          date: _payload.date,
+          offset,
+          addresses: activeAddrs,
+        },
+        {
+          removeOnComplete: true,
+          removeOnFail: {
+            count: 3,
           },
-          {
-            removeOnComplete: true,
-            removeOnFail: {
-              count: 3,
-            },
-          }
-        );
-      } else {
-        const [dailyTxs, uniqueAddrs, prevDailyStat] = await Promise.all([
-          Transaction.query()
-            .count('id')
-            .where('timestamp', '>=', startTime)
-            .andWhere('timestamp', '<', endTime)
-            .andWhere('code', 0),
-          Account.query().count('id'),
-          DailyStatistics.query().findOne('date', startTime),
-        ]);
+        }
+      );
+    } else {
+      const [dailyTxs, uniqueAddrs, prevDailyStat] = await Promise.all([
+        Transaction.query()
+          .count('id')
+          .where('timestamp', '>=', startTime)
+          .andWhere('timestamp', '<', endTime)
+          .andWhere('code', 0),
+        Account.query().count('id'),
+        DailyStatistics.query().findOne('date', startTime),
+      ]);
 
-        const dailyStat = DailyStatistics.fromJson({
-          daily_txs: dailyTxs[0].count,
-          daily_active_addresses: _payload.addresses.length,
-          unique_addresses: Number(uniqueAddrs[0].count),
-          unique_addresses_increase: prevDailyStat
-            ? uniqueAddrs[0].count - prevDailyStat.unique_addresses
-            : 0,
-          date: startTime.toISOString(),
+      const dailyStat = DailyStatistics.fromJson({
+        daily_txs: dailyTxs[0].count,
+        daily_active_addresses: _payload.addresses.length,
+        unique_addresses: Number(uniqueAddrs[0].count),
+        unique_addresses_increase: prevDailyStat
+          ? uniqueAddrs[0].count - prevDailyStat.unique_addresses
+          : 0,
+        date: startTime.toISOString(),
+      });
+
+      this.logger.info(`Insert new daily statistic for date ${startTime}`);
+      await DailyStatistics.query()
+        .insert(dailyStat)
+        .catch((error) => {
+          this.logger.error('Error insert new daily statistic record');
+          this.logger.error(error);
         });
-
-        this.logger.info(`Insert new daily statistic for date ${startTime}`);
-        await DailyStatistics.query()
-          .insert(dailyStat)
-          .catch((error) => {
-            this.logger.error('Error insert new daily statistic record');
-            this.logger.error(error);
-          });
-      }
-    } catch (error) {
-      this.logger.error(error);
     }
   }
 
