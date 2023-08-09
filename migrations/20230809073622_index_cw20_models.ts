b746b0e25319499381492def21bafe1528c4ccdb
phamphong9981 <phamphong9981@gmail.com>
2023-08-09 16:42:45 +0700

    refactor: code

--- migrations/20230809073622_index_cw20_models.ts
+++ migrations/20230809073622_index_cw20_models.ts
@@ -4,12 +4,24 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable('cw20_activity', (table) => {
     table.index('from');
     table.index('to');
+    table.index('action');
+  });
+  await knex.schema.alterTable('cw721_activity', (table) => {
+    table.index('from');
+    table.index('to');
+    table.index('action');
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.alterTable('proposal', (table) => {
+  await knex.schema.alterTable('cw20_activity', (table) => {
     table.dropIndex('from');
     table.dropIndex('to');
+    table.dropIndex('action');
+  });
+  await knex.schema.alterTable('cw721_activity', (table) => {
+    table.dropIndex('from');
+    table.dropIndex('to');
+    table.dropIndex('action');
   });
 }
