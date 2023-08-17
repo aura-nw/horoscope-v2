4303356e5e7c6971bea414e101b066807e5f2ee0
Phan Anh Tuan <fibonacci998@gmail.com>
2023-08-17 11:25:05 +0700

    feat: add test mapping by authz tx

--- test/unit/services/crawl-transaction/parse_transaction.spec.ts
+++ test/unit/services/crawl-transaction/parse_transaction.spec.ts
@@ -154,81 +154,394 @@ export default class CrawlTransactionTest {
     // );
   }
 
-  @Test('Mapping event and log')
-  public async testMappingEventToLog() {
-    const arrDest = {
-      index: 3,
-      type: 'message',
+  arrDest = {
+    index: 3,
+    type: 'coin_received',
+    attributes: [
+      { key: 'receiver', value: 'aura162x2llsxzxmavtyuxjesceewmy4wvrp79ndcrw' },
+      { key: 'amount', value: '1341uaura' },
+      { key: 'authz_msg_index', value: '0' },
+      { key: 'receiver', value: 'aura1fl48vsnmsdzcv85q5d2q4z5ajdha8yu3wd7dmw' },
+      { key: 'amount', value: '909uaura' },
+      { key: 'authz_msg_index', value: '0' },
+      { key: 'receiver', value: 'aura1ujv2gmfwrwzj504ntggqld0q5euafp76vgx5lj' },
+      { key: 'amount', value: '154uaura' },
+      { key: 'authz_msg_index', value: '1' },
+      { key: 'receiver', value: 'aura1fl48vsnmsdzcv85q5d2q4z5ajdha8yu3wd7dmw' },
+      { key: 'amount', value: '104uaura' },
+      { key: 'authz_msg_index', value: '1' },
+      { key: 'receiver', value: 'aura177cgzmjve5m0je6yjukcj4mmmwj8p4dkqekglz' },
+      { key: 'amount', value: '4511uaura' },
+      { key: 'authz_msg_index', value: '2' },
+      { key: 'receiver', value: 'aura1fl48vsnmsdzcv85q5d2q4z5ajdha8yu3wd7dmw' },
+      { key: 'amount', value: '3056uaura' },
+      { key: 'authz_msg_index', value: '2' },
+      { key: 'receiver', value: 'aura1rqll2d4wyylvl03ht6mhglswj46gkcr3ksvkm7' },
+      { key: 'amount', value: '3617uaura' },
+      { key: 'authz_msg_index', value: '3' },
+      { key: 'receiver', value: 'aura1fl48vsnmsdzcv85q5d2q4z5ajdha8yu3wd7dmw' },
+      { key: 'amount', value: '2451uaura' },
+      { key: 'authz_msg_index', value: '3' },
+      { key: 'receiver', value: 'aura1a6x0znjhztz73tq07gjvzt9ru99866jm665w9p' },
+      { key: 'amount', value: '5417uaura' },
+      { key: 'authz_msg_index', value: '4' },
+      { key: 'receiver', value: 'aura1fl48vsnmsdzcv85q5d2q4z5ajdha8yu3wd7dmw' },
+      { key: 'amount', value: '3670uaura' },
+      { key: 'authz_msg_index', value: '4' },
+      { key: 'receiver', value: 'aura1q93xkwtfv7nut0eqjjws377wkjk97265zsxlx6' },
+      { key: 'amount', value: '30uaura' },
+      { key: 'authz_msg_index', value: '5' },
+      { key: 'receiver', value: 'aura1fl48vsnmsdzcv85q5d2q4z5ajdha8yu3wd7dmw' },
+      { key: 'amount', value: '20uaura' },
+      { key: 'authz_msg_index', value: '5' },
+      { key: 'receiver', value: 'aura1xk6nnn0gen9n9fduz0t3twyzt8c2uzedy2545a' },
+      { key: 'amount', value: '2797uaura' },
+      { key: 'authz_msg_index', value: '6' },
+      { key: 'receiver', value: 'aura1fl48vsnmsdzcv85q5d2q4z5ajdha8yu3wd7dmw' },
+      { key: 'amount', value: '1895uaura' },
+      { key: 'authz_msg_index', value: '6' },
+      { key: 'receiver', value: 'aura1f3qxww8pnx0xrea7e03ffxnlau0fk5ufs3v0zj' },
+      { key: 'amount', value: '2457uaura' },
+      { key: 'authz_msg_index', value: '7' },
+      { key: 'receiver', value: 'aura1fl48vsnmsdzcv85q5d2q4z5ajdha8yu3wd7dmw' },
+      { key: 'amount', value: '1665uaura' },
+      { key: 'authz_msg_index', value: '7' },
+      { key: 'receiver', value: 'aura1fwtkqe4yp652svrj5lzdu9lnykysh947msc4xq' },
+      { key: 'amount', value: '2589uaura' },
+      { key: 'authz_msg_index', value: '8' },
+      { key: 'receiver', value: 'aura1fl48vsnmsdzcv85q5d2q4z5ajdha8yu3wd7dmw' },
+      { key: 'amount', value: '1754uaura' },
+      { key: 'authz_msg_index', value: '8' },
+      { key: 'receiver', value: 'aura1j6kwc05lw0p8e08ce3r5z5qlygjzu0aq095scc' },
+      { key: 'amount', value: '212uaura' },
+      { key: 'authz_msg_index', value: '9' },
+      { key: 'receiver', value: 'aura1fl48vsnmsdzcv85q5d2q4z5ajdha8yu3wd7dmw' },
+      { key: 'amount', value: '143uaura' },
+      { key: 'authz_msg_index', value: '9' },
+      { key: 'receiver', value: 'aura15f6wn3nymdnhnh5ddlqletuptjag09tryrtpq5' },
+      { key: 'amount', value: '1689uaura' },
+      { key: 'authz_msg_index', value: '10' },
+      { key: 'receiver', value: 'aura1fl48vsnmsdzcv85q5d2q4z5ajdha8yu3wd7dmw' },
+      { key: 'amount', value: '1144uaura' },
+      { key: 'authz_msg_index', value: '10' },
+      { key: 'receiver', value: 'aura1efq5q4uzn583nh5mauzc5cmgms53w9l6vs5dxz' },
+      { key: 'amount', value: '105uaura' },
+      { key: 'authz_msg_index', value: '11' },
+      { key: 'receiver', value: 'aura1fl48vsnmsdzcv85q5d2q4z5ajdha8yu3wd7dmw' },
+      { key: 'amount', value: '71uaura' },
+      { key: 'authz_msg_index', value: '11' },
+    ],
+  };
+
+  arrSrc: any[] = [
+    {
+      type: 'coin_received',
       attributes: [
         {
-          key: 'action',
-          value: '/cosmos.staking.v1beta1.MsgDelegate',
+          key: 'cmVjZWl2ZXI=',
+          value: 'YXVyYTE3eHBmdmFrbTJhbWc5NjJ5bHM2Zjg0ejNrZWxsOGM1bHQwNXpmeQ==',
         },
-        {
-          key: 'sender',
-          value: 'aura1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8ufn7tx',
-        },
-        {
-          key: 'module',
-          value: 'staking',
-        },
-        {
-          key: 'sender',
-          value: 'aura15x4v36r6rl73nhn9h0954mwp42sawrc25f0rnx',
-        },
+        { key: 'YW1vdW50', value: 'ODk2dWF1cmE=' },
       ],
-    };
-    const arrSrc: any[] = [
-      {
-        // this event is not in log (arrDest), so it doesn't has checkedIndex
-        type: 'message',
-        attributes: [
-          {
-            key: 'c2VuZGVy',
-            value:
-              'YXVyYTE1eDR2MzZyNnJsNzNuaG45aDA5NTRtd3A0MnNhd3JjMjVmMHJueA==',
-          },
-        ],
-      },
-      {
-        checkedIndex: 3,
-        type: 'message',
-        attributes: [
-          {
-            key: 'YWN0aW9u',
-            value: 'L2Nvc21vcy5zdGFraW5nLnYxYmV0YTEuTXNnRGVsZWdhdGU=',
-          },
-        ],
-      },
-      {
-        checkedIndex: 3,
-        type: 'message',
-        attributes: [
-          {
-            key: 'c2VuZGVy',
-            value:
-              'YXVyYTFqdjY1czNncnFmNnY2amwzZHA0dDZjOXQ5cms5OWNkOHVmbjd0eA==',
-          },
-        ],
-      },
-      {
-        checkedIndex: 3,
-        type: 'message',
-        attributes: [
-          {
-            key: 'bW9kdWxl',
-            value: 'c3Rha2luZw==',
-          },
-          {
-            key: 'c2VuZGVy',
-            value:
-              'YXVyYTE1eDR2MzZyNnJsNzNuaG45aDA5NTRtd3A0MnNhd3JjMjVmMHJueA==',
-          },
-        ],
-      },
-    ];
-    this.crawlTxService?.mappingEventToLog(arrDest, arrSrc, arrDest.index);
-    arrSrc.forEach((item) => {
+    },
+    {
+      type: 'coin_received',
+      checkedIndex: 3,
+      attributes: [
+        {
+          key: 'cmVjZWl2ZXI=',
+          value: 'YXVyYTE2MngybGxzeHp4bWF2dHl1eGplc2NlZXdteTR3dnJwNzluZGNydw==',
+        },
+        { key: 'YW1vdW50', value: 'MTM0MXVhdXJh' },
+        { key: 'YXV0aHpfbXNnX2luZGV4', value: 'MA==' },
+      ],
+    },
+    {
+      type: 'coin_received',
+      checkedIndex: 3,
+      attributes: [
+        {
+          key: 'cmVjZWl2ZXI=',
+          value: 'YXVyYTFmbDQ4dnNubXNkemN2ODVxNWQycTR6NWFqZGhhOHl1M3dkN2Rtdw==',
+        },
+        { key: 'YW1vdW50', value: 'OTA5dWF1cmE=' },
+        { key: 'YXV0aHpfbXNnX2luZGV4', value: 'MA==' },
+      ],
+    },
+    {
+      type: 'coin_received',
+      checkedIndex: 3,
+      attributes: [
+        {
+          key: 'cmVjZWl2ZXI=',
+          value: 'YXVyYTF1anYyZ21md3J3emo1MDRudGdncWxkMHE1ZXVhZnA3NnZneDVsag==',
+        },
+        { key: 'YW1vdW50', value: 'MTU0dWF1cmE=' },
+        { key: 'YXV0aHpfbXNnX2luZGV4', value: 'MQ==' },
+      ],
+    },
+    {
+      type: 'coin_received',
+      checkedIndex: 3,
+      attributes: [
+        {
+          key: 'cmVjZWl2ZXI=',
+          value: 'YXVyYTFmbDQ4dnNubXNkemN2ODVxNWQycTR6NWFqZGhhOHl1M3dkN2Rtdw==',
+        },
+        { key: 'YW1vdW50', value: 'MTA0dWF1cmE=' },
+        { key: 'YXV0aHpfbXNnX2luZGV4', value: 'MQ==' },
+      ],
+    },
+    {
+      type: 'coin_received',
+      checkedIndex: 3,
+      attributes: [
+        {
+          key: 'cmVjZWl2ZXI=',
+          value: 'YXVyYTE3N2Nnem1qdmU1bTBqZTZ5anVrY2o0bW1td2o4cDRka3Fla2dseg==',
+        },
+        { key: 'YW1vdW50', value: 'NDUxMXVhdXJh' },
+        { key: 'YXV0aHpfbXNnX2luZGV4', value: 'Mg==' },
+      ],
+    },
+    {
+      type: 'coin_received',
+      checkedIndex: 3,
+      attributes: [
+        {
+          key: 'cmVjZWl2ZXI=',
+          value: 'YXVyYTFmbDQ4dnNubXNkemN2ODVxNWQycTR6NWFqZGhhOHl1M3dkN2Rtdw==',
+        },
+        { key: 'YW1vdW50', value: 'MzA1NnVhdXJh' },
+        { key: 'YXV0aHpfbXNnX2luZGV4', value: 'Mg==' },
+      ],
+    },
+    {
+      type: 'coin_received',
+      checkedIndex: 3,
+      attributes: [
+        {
+          key: 'cmVjZWl2ZXI=',
+          value: 'YXVyYTFycWxsMmQ0d3l5bHZsMDNodDZtaGdsc3dqNDZna2NyM2tzdmttNw==',
+        },
+        { key: 'YW1vdW50', value: 'MzYxN3VhdXJh' },
+        { key: 'YXV0aHpfbXNnX2luZGV4', value: 'Mw==' },
+      ],
+    },
+    {
+      type: 'coin_received',
+      checkedIndex: 3,
+      attributes: [
+        {
+          key: 'cmVjZWl2ZXI=',
+          value: 'YXVyYTFmbDQ4dnNubXNkemN2ODVxNWQycTR6NWFqZGhhOHl1M3dkN2Rtdw==',
+        },
+        { key: 'YW1vdW50', value: 'MjQ1MXVhdXJh' },
+        { key: 'YXV0aHpfbXNnX2luZGV4', value: 'Mw==' },
+      ],
+    },
+    {
+      type: 'coin_received',
+      checkedIndex: 3,
+      attributes: [
+        {
+          key: 'cmVjZWl2ZXI=',
+          value: 'YXVyYTFhNngwem5qaHp0ejczdHEwN2dqdnp0OXJ1OTk4NjZqbTY2NXc5cA==',
+        },
+        { key: 'YW1vdW50', value: 'NTQxN3VhdXJh' },
+        { key: 'YXV0aHpfbXNnX2luZGV4', value: 'NA==' },
+      ],
+    },
+    {
+      type: 'coin_received',
+      checkedIndex: 3,
+      attributes: [
+        {
+          key: 'cmVjZWl2ZXI=',
+          value: 'YXVyYTFmbDQ4dnNubXNkemN2ODVxNWQycTR6NWFqZGhhOHl1M3dkN2Rtdw==',
+        },
+        { key: 'YW1vdW50', value: 'MzY3MHVhdXJh' },
+        { key: 'YXV0aHpfbXNnX2luZGV4', value: 'NA==' },
+      ],
+    },
+    {
+      type: 'coin_received',
+      checkedIndex: 3,
+      attributes: [
+        {
+          key: 'cmVjZWl2ZXI=',
+          value: 'YXVyYTFxOTN4a3d0ZnY3bnV0MGVxamp3czM3N3drams5NzI2NXpzeGx4Ng==',
+        },
+        { key: 'YW1vdW50', value: 'MzB1YXVyYQ==' },
+        { key: 'YXV0aHpfbXNnX2luZGV4', value: 'NQ==' },
+      ],
+    },
+    {
+      type: 'coin_received',
+      checkedIndex: 3,
+      attributes: [
+        {
+          key: 'cmVjZWl2ZXI=',
+          value: 'YXVyYTFmbDQ4dnNubXNkemN2ODVxNWQycTR6NWFqZGhhOHl1M3dkN2Rtdw==',
+        },
+        { key: 'YW1vdW50', value: 'MjB1YXVyYQ==' },
+        { key: 'YXV0aHpfbXNnX2luZGV4', value: 'NQ==' },
+      ],
+    },
+    {
+      type: 'coin_received',
+      checkedIndex: 3,
+      attributes: [
+        {
+          key: 'cmVjZWl2ZXI=',
+          value: 'YXVyYTF4azZubm4wZ2VuOW45ZmR1ejB0M3R3eXp0OGMydXplZHkyNTQ1YQ==',
+        },
+        { key: 'YW1vdW50', value: 'Mjc5N3VhdXJh' },
+        { key: 'YXV0aHpfbXNnX2luZGV4', value: 'Ng==' },
+      ],
+    },
+    {
+      type: 'coin_received',
+      checkedIndex: 3,
+      attributes: [
+        {
+          key: 'cmVjZWl2ZXI=',
+          value: 'YXVyYTFmbDQ4dnNubXNkemN2ODVxNWQycTR6NWFqZGhhOHl1M3dkN2Rtdw==',
+        },
+        { key: 'YW1vdW50', value: 'MTg5NXVhdXJh' },
+        { key: 'YXV0aHpfbXNnX2luZGV4', value: 'Ng==' },
+      ],
+    },
+    {
+      type: 'coin_received',
+      checkedIndex: 3,
+      attributes: [
+        {
+          key: 'cmVjZWl2ZXI=',
+          value: 'YXVyYTFmM3F4d3c4cG54MHhyZWE3ZTAzZmZ4bmxhdTBmazV1ZnMzdjB6ag==',
+        },
+        { key: 'YW1vdW50', value: 'MjQ1N3VhdXJh' },
+        { key: 'YXV0aHpfbXNnX2luZGV4', value: 'Nw==' },
+      ],
+    },
+    {
+      type: 'coin_received',
+      checkedIndex: 3,
+      attributes: [
+        {
+          key: 'cmVjZWl2ZXI=',
+          value: 'YXVyYTFmbDQ4dnNubXNkemN2ODVxNWQycTR6NWFqZGhhOHl1M3dkN2Rtdw==',
+        },
+        { key: 'YW1vdW50', value: 'MTY2NXVhdXJh' },
+        { key: 'YXV0aHpfbXNnX2luZGV4', value: 'Nw==' },
+      ],
+    },
+    {
+      type: 'coin_received',
+      checkedIndex: 3,
+      attributes: [
+        {
+          key: 'cmVjZWl2ZXI=',
+          value: 'YXVyYTFmd3RrcWU0eXA2NTJzdnJqNWx6ZHU5bG55a3lzaDk0N21zYzR4cQ==',
+        },
+        { key: 'YW1vdW50', value: 'MjU4OXVhdXJh' },
+        { key: 'YXV0aHpfbXNnX2luZGV4', value: 'OA==' },
+      ],
+    },
+    {
+      type: 'coin_received',
+      checkedIndex: 3,
+      attributes: [
+        {
+          key: 'cmVjZWl2ZXI=',
+          value: 'YXVyYTFmbDQ4dnNubXNkemN2ODVxNWQycTR6NWFqZGhhOHl1M3dkN2Rtdw==',
+        },
+        { key: 'YW1vdW50', value: 'MTc1NHVhdXJh' },
+        { key: 'YXV0aHpfbXNnX2luZGV4', value: 'OA==' },
+      ],
+    },
+    {
+      type: 'coin_received',
+      checkedIndex: 3,
+      attributes: [
+        {
+          key: 'cmVjZWl2ZXI=',
+          value: 'YXVyYTFqNmt3YzA1bHcwcDhlMDhjZTNyNXo1cWx5Z2p6dTBhcTA5NXNjYw==',
+        },
+        { key: 'YW1vdW50', value: 'MjEydWF1cmE=' },
+        { key: 'YXV0aHpfbXNnX2luZGV4', value: 'OQ==' },
+      ],
+    },
+    {
+      type: 'coin_received',
+      checkedIndex: 3,
+      attributes: [
+        {
+          key: 'cmVjZWl2ZXI=',
+          value: 'YXVyYTFmbDQ4dnNubXNkemN2ODVxNWQycTR6NWFqZGhhOHl1M3dkN2Rtdw==',
+        },
+        { key: 'YW1vdW50', value: 'MTQzdWF1cmE=' },
+        { key: 'YXV0aHpfbXNnX2luZGV4', value: 'OQ==' },
+      ],
+    },
+    {
+      type: 'coin_received',
+      checkedIndex: 3,
+      attributes: [
+        {
+          key: 'cmVjZWl2ZXI=',
+          value: 'YXVyYTE1ZjZ3bjNueW1kbmhuaDVkZGxxbGV0dXB0amFnMDl0cnlydHBxNQ==',
+        },
+        { key: 'YW1vdW50', value: 'MTY4OXVhdXJh' },
+        { key: 'YXV0aHpfbXNnX2luZGV4', value: 'MTA=' },
+      ],
+    },
+    {
+      type: 'coin_received',
+      checkedIndex: 3,
+      attributes: [
+        {
+          key: 'cmVjZWl2ZXI=',
+          value: 'YXVyYTFmbDQ4dnNubXNkemN2ODVxNWQycTR6NWFqZGhhOHl1M3dkN2Rtdw==',
+        },
+        { key: 'YW1vdW50', value: 'MTE0NHVhdXJh' },
+        { key: 'YXV0aHpfbXNnX2luZGV4', value: 'MTA=' },
+      ],
+    },
+    {
+      type: 'coin_received',
+      checkedIndex: 3,
+      attributes: [
+        {
+          key: 'cmVjZWl2ZXI=',
+          value: 'YXVyYTFlZnE1cTR1em41ODNuaDVtYXV6YzVjbWdtczUzdzlsNnZzNWR4eg==',
+        },
+        { key: 'YW1vdW50', value: 'MTA1dWF1cmE=' },
+        { key: 'YXV0aHpfbXNnX2luZGV4', value: 'MTE=' },
+      ],
+    },
+    {
+      type: 'coin_received',
+      checkedIndex: 3,
+      attributes: [
+        {
+          key: 'cmVjZWl2ZXI=',
+          value: 'YXVyYTFmbDQ4dnNubXNkemN2ODVxNWQycTR6NWFqZGhhOHl1M3dkN2Rtdw==',
+        },
+        { key: 'YW1vdW50', value: 'NzF1YXVyYQ==' },
+        { key: 'YXV0aHpfbXNnX2luZGV4', value: 'MTE=' },
+      ],
+    },
+  ];
+
+  @Test('Mapping event and log')
+  public async testMappingEventToLog() {
+    this.crawlTxService?.mappingEventToLog(
+      this.arrDest,
+      this.arrSrc,
+      this.arrDest.index
+    );
+    this.arrSrc.forEach((item) => {
       expect(item.checkedIndex).toEqual(item.msg_index);
     });
   }
