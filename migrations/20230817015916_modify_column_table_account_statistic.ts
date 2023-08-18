ef0f7dd3d42949eb16278c51b793631429c3f41c
Phan Anh Tuan <fibonacci998@gmail.com>
2023-08-18 14:45:32 +0700

    fix: remove drop index in modify account statistic table

--- migrations/20230817015916_modify_column_table_account_statistic.ts
+++ migrations/20230817015916_modify_column_table_account_statistic.ts
@@ -2,20 +2,12 @@ import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable('account_statistics', (table) => {
-    table.dropIndex('amount_sent');
-    table.dropIndex('amount_received');
-    table.dropIndex('tx_sent');
-    table.dropIndex('gas_used');
     table.integer('tx_sent').alter();
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.alterTable('account_statistics', (table) => {
-    table.index('amount_sent');
-    table.index('amount_received');
-    table.index('tx_sent');
-    table.index('gas_used');
     table.bigint('tx_sent').alter();
   });
 }
