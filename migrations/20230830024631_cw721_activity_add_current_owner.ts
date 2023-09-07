ced56b9ed50a78dc71175016d459988d3ab29f09
phamphong9981 <phamphong9981@gmail.com>
2023-09-07 17:27:00 +0700

    refactor: review code

--- migrations/20230830024631_cw721_activity_add_current_owner.ts
+++ migrations/20230830024631_cw721_activity_add_current_owner.ts
@@ -1,7 +1,8 @@
 import { Knex } from 'knex';
 import CW721Activity from '../src/models/cw721_tx';
-import _ from 'lodash';
+import _, { Dictionary } from 'lodash';
 import { CW721_ACTION } from '../src/services/cw721/cw721.service';
+
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable('cw721_activity', (table) => {
     table.string('owner');
@@ -9,27 +10,28 @@ export async function up(knex: Knex): Promise<void> {
   await knex.transaction(async (trx) => {
     const activities = await CW721Activity.query()
       .joinRelated('event')
-      .orderBy('event.block_height', 'asc')
+      .orderBy('event.id', 'asc')
       .transacting(trx);
-    const done: CW721Activity[] = [];
+    const latestOwners: Dictionary<string> = {};
     activities.forEach((activity) => {
-      const latestActivity = _.findLast(done, function (e) {
-        return (
-          [
-            CW721_ACTION.MINT,
-            CW721_ACTION.TRANSFER,
-            CW721_ACTION.SEND_NFT,
-          ].includes(e.action) &&
-          e.cw721_contract_id === activity.cw721_contract_id &&
-          e.cw721_token_id === activity.cw721_token_id
-        );
-      });
-      if (latestActivity) {
-        activity.owner = latestActivity.to;
+      const latestOwner =
+        latestOwners[
+          activity.cw721_contract_id + '_' + activity.cw721_token_id
+        ];
+      if (latestOwner) {
+        activity.owner = latestOwner;
       } else {
         activity.owner = activity.to;
       }
-      done.push(activity);
+      if (
+        activity.action === CW721_ACTION.MINT ||
+        activity.action === CW721_ACTION.TRANSFER ||
+        activity.action === CW721_ACTION.SEND_NFT
+      ) {
+        latestOwners[
+          activity.cw721_contract_id + '_' + activity.cw721_token_id
+        ] = activity.to;
+      }
     });
     if (activities.length > 0) {
       await CW721Activity.query()
