e42a9c1c5d3d7a17b99fdbf60a2e4924b577a638
phamphong9981 <phamphong9981@gmail.com>
2023-09-13 14:14:47 +0700

    fix: test

--- src/services/crawl-cosmwasm/crawl_smart_contract.service.ts
+++ src/services/crawl-cosmwasm/crawl_smart_contract.service.ts
@@ -289,7 +289,6 @@ export default class CrawlSmartContractService extends BullableService {
             instantiate_hash: contract.hash,
             instantiate_height: contract.height,
             version: null,
-            label: '',
           })
         );
       });
@@ -323,7 +322,7 @@ export default class CrawlSmartContractService extends BullableService {
             10
           );
           contract.creator = contractInfos[index]?.contractInfo?.creator || '';
-          contract.label = contractInfos[index]?.contractInfo?.label || '';
+          contract.label = contractInfos[index]?.contractInfo?.label;
           codeIds.push(
             parseInt(
               contractInfos[index]?.contractInfo?.codeId.toString() || '0',
