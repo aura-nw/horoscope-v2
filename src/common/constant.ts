69103dec07f96287df4055146705aa0330f9c5d3
Phan Anh Tuan <fibonacci998@gmail.com>
2023-09-18 16:39:22 +0700

    feat: create job to create composite index in event_attribute table

--- src/common/constant.ts
+++ src/common/constant.ts
@@ -76,6 +76,8 @@ export const BULL_JOB_NAME = {
   CRAWL_IBC_APP: 'crawl:ibc-app',
   REINDEX_CW20_CONTRACT: 'reindex:cw20-contract',
   REINDEX_CW20_HISTORY: 'reindex:cw20-history',
+  JOB_CREATE_COMPOSITE_INDEX_ATTR_PARTITION:
+    'job:create-index-composite-attr-partition',
 };
 
 export const SERVICE = {
@@ -231,6 +233,10 @@ export const SERVICE = {
       ReDecodeTx: {
         key: 'ReDecodeTx',
         path: 'v1.ReDecodeTx',
+      },
+      CreateIndexCompositeAttrPartition: {
+        key: 'CreateIndexCompositeAttrPartition',
+        path: 'v1.CreateIndexCompositeAttrPartition',
       },
     },
     CrawlIBCTaoService: {
