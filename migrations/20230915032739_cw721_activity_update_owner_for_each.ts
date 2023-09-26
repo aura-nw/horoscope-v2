192caf3e86a084d3db66de13d35f688e826d8b10
Phạm Thành Phong <49814372+phamphong9981@users.noreply.github.com>
2023-09-26 16:21:19 +0700

    Refactor/cw721 ( develop ) (#386)
    * refactor: cw721
    
    * test: full test
    
    * feat: migration
    
    * fix: lint
    
    * fix: test
    
    * fix: test
    
    * refactor: test
    
    * refactor: code
    
    * refactor: test
    
    * fix: test
    
    * test: code
    
    * test: code
    
    * refactor: code
    
    * refactor: review
    
    * refactor: review
    
    * refactor: clean redundance get DB field

---
+++ migrations/20230915032739_cw721_activity_update_owner_for_each.ts
@@ -0,0 +1,46 @@
+import { Knex } from 'knex';
+import CW721Activity from '../src/models/cw721_tx';
+import _, { Dictionary } from 'lodash';
+import { CW721_ACTION } from '../src/services/cw721/cw721_handler';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex('cw721_activity').update({
+    sender: knex.ref('from'),
+  });
+  await knex.transaction(async (trx) => {
+    const activities = await CW721Activity.query()
+      .joinRelated('event')
+      .orderBy('event.id', 'asc')
+      .transacting(trx);
+    const latestOwners: Dictionary<string | null> = {};
+    activities.forEach((activity) => {
+      const latestOwner =
+        latestOwners[
+          activity.cw721_contract_id + '_' + activity.cw721_token_id
+        ];
+      if (latestOwner) {
+        activity.from = latestOwner;
+      } else {
+        activity.from = null;
+      }
+      if (
+        activity.action === CW721_ACTION.MINT ||
+        activity.action === CW721_ACTION.TRANSFER ||
+        activity.action === CW721_ACTION.SEND_NFT
+      ) {
+        latestOwners[
+          activity.cw721_contract_id + '_' + activity.cw721_token_id
+        ] = activity.to;
+      }
+    });
+    if (activities.length > 0) {
+      await CW721Activity.query()
+        .insert(activities)
+        .onConflict(['id'])
+        .merge()
+        .transacting(trx);
+    }
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {}
