f9979ed28c4c17e09ef030c28a760312ca682938
phamphong9981 <phamphong9981@gmail.com>
2023-09-26 16:05:49 +0700

    refactor: cw721 and cw20 add smart contract address

--- migrations/20230926080752_cw721_and_cw20_add_smart_contract_address.ts
+++ migrations/20230926080752_cw721_and_cw20_add_smart_contract_address.ts
@@ -14,34 +14,39 @@ export async function up(knex: Knex): Promise<void> {
     const cw721Contracts = await CW721Contract.query()
       .transacting(trx)
       .withGraphFetched('smart_contract');
-    cw721Contracts.forEach((cw721Contract) => {
-      cw721Contract.smart_contract_address =
-        cw721Contract.smart_contract.address;
-    });
-    await CW721Contract.query()
-      .transacting(trx)
-      .insert(
-        cw721Contracts.map((cw721Contract) =>
-          _.omit(cw721Contract, 'smart_contract')
+    if (cw721Contracts.length > 0) {
+      cw721Contracts.forEach((cw721Contract) => {
+        cw721Contract.smart_contract_address =
+          cw721Contract.smart_contract.address;
+      });
+      await CW721Contract.query()
+        .transacting(trx)
+        .insert(
+          cw721Contracts.map((cw721Contract) =>
+            _.omit(cw721Contract, 'smart_contract')
+          )
         )
-      )
-      .onConflict('id')
-      .merge();
+        .onConflict('id')
+        .merge();
+    }
     const cw20Contracts = await Cw20Contract.query()
       .transacting(trx)
       .withGraphFetched('smart_contract');
-    cw20Contracts.forEach((cw20Contract) => {
-      cw20Contract.smart_contract_address = cw20Contract.smart_contract.address;
-    });
-    await Cw20Contract.query()
-      .transacting(trx)
-      .insert(
-        cw20Contracts.map((cw20Contract) =>
-          _.omit(cw20Contract, 'smart_contract')
+    if (cw20Contracts.length > 0) {
+      cw20Contracts.forEach((cw20Contract) => {
+        cw20Contract.smart_contract_address =
+          cw20Contract.smart_contract.address;
+      });
+      await Cw20Contract.query()
+        .transacting(trx)
+        .insert(
+          cw20Contracts.map((cw20Contract) =>
+            _.omit(cw20Contract, 'smart_contract')
+          )
         )
-      )
-      .onConflict('id')
-      .merge();
+        .onConflict('id')
+        .merge();
+    }
   });
 }
 
