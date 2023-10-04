75baf0f231a9d4f0e3be643db07ab717ae0ffb65
phamphong9981 <phamphong9981@gmail.com>
2023-10-04 16:46:33 +0700

    fix: queue manager stopAll return Promise

--- test/unit/services/api-gateways/cw721_admin.spec.ts
+++ test/unit/services/api-gateways/cw721_admin.spec.ts
@@ -24,10 +24,10 @@ export default class Cw721AdminTest {
 
   @BeforeAll()
   async initSuite() {
+    await this.broker.start();
     await this.cw721Admin.getQueueManager().stopAll();
     await this.cw721HandlerService.getQueueManager().stopAll();
     await this.cw721Reindex.getQueueManager().stopAll();
-    await this.broker.start();
   }
 
   @AfterAll()
