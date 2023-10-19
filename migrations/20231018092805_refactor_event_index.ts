a5b3a8c10c3d02d2a63883fc47ae8cc4e9446225
minhnv78 <minhnv78@fpt.com>
2023-10-19 10:17:36 +0700

    fix: seperate logic of migration index on transaction and event table

--- migrations/20231018092805_refactor_event_index.ts
+++ migrations/20231018092805_refactor_event_index.ts
@@ -5,17 +5,9 @@ export async function up(knex: Knex): Promise<void> {
     table.index('block_height', 'brin_idx_block_height_event', 'brin');
     table.index('tx_id', 'brin_idx_tx_id_event', 'brin');
   });
-  await knex.schema.alterTable('event', (table) => {
-    table.dropIndex('block_height', 'event_block_height_index');
-    table.dropIndex('tx_id', 'transaction_event_tx_id_index');
-  });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.alterTable('event', (table) => {
-    table.index('block_height', 'event_block_height_index', 'btree');
-    table.index('tx_id', 'transaction_event_tx_id_index', 'btree');
-  });
   await knex.schema.alterTable('event', (table) => {
     table.dropIndex('block_height', 'brin_idx_block_height_event');
     table.dropIndex('tx_id', 'brin_idx_tx_id_event');
