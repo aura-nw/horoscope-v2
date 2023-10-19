f2c6dcb17e1fb57a41a6b628f35125f42c80577b
matthew-nguyen-20032023 <129312342+matthew-nguyen-20032023@users.noreply.github.com>
2023-10-19 10:29:35 +0700

    Refactor/refactor btree index develop (#428)
    * fix: refactor index of transaction table
    
    * fix: refactor index of event table
    
    * fix: seperate logic of migration index on transaction and event table
    
    ---------
    
    Co-authored-by: minhnv78 <minhnv78@fpt.com>

--- migrations/20231019031400_drop_old_btree_index_transaction.ts
+++ migrations/20231019031400_drop_old_btree_index_transaction.ts
@@ -2,14 +2,14 @@ import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable('transaction', (table) => {
-    table.index('height', 'brin_idx_height_transaction', 'brin');
-    table.index('timestamp', 'brin_idx_timestamp_transaction', 'brin');
+    table.dropIndex('height', 'transaction_height_index');
+    table.dropIndex('timestamp', 'transaction_timestamp_index');
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.alterTable('transaction', (table) => {
-    table.dropIndex('height', 'brin_idx_height_transaction');
-    table.dropIndex('timestamp', 'brin_idx_timestamp_transaction');
+    table.index('height', 'transaction_height_index', 'btree');
+    table.index('timestamp', 'transaction_timestamp_index', 'btree');
   });
 }
