ca3aa4b165e631181e9589dfbf3a56b4ea21a460
Phan Anh Tuan <fibonacci998@gmail.com>
2023-10-27 15:55:48 +0700

    feat: test add set statement_timeout to overwrite param default in db

--- migrations/20231018092805_refactor_event_index.ts
+++ migrations/20231018092805_refactor_event_index.ts
@@ -1,6 +1,7 @@
 import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
+  await knex.raw('set statement_timeout to 600000');
   await knex.schema.alterTable('event', (table) => {
     table.index('block_height', 'brin_idx_block_height_event', 'brin');
     table.index('tx_id', 'brin_idx_tx_id_event', 'brin');
