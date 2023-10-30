df8b947ea268583fef562a737afc176fa6e95660
matthew.nguyen.20.03.2023 <matthew.nguyen.20.03.2023@gmail.com>
2023-10-30 15:33:12 +0700

    fix: improve count transaction

--- migrations/20231027040120_create-table-statistics.ts
+++ migrations/20231027040120_create-table-statistics.ts
@@ -2,8 +2,9 @@ import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('statistics', (table: any) => {
+    table.increments();
     // Name for statistic such as total_transaction, total_account, and so on
-    table.string('key').primary();
+    table.string('key').notNullable().unique().index();
     // Value of key name above
     table.bigint('value').notNullable().defaultTo(0);
     // Determine statistic has been counted since something (block, timestamp, date, and so on)
