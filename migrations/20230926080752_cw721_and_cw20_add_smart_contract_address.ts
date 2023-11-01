9cd9b0fefc47070717499db5abb15e42c37bafce
phamphong9981 <phamphong9981@gmail.com>
2023-11-01 09:24:04 +0700

    refactor: review

--- migrations/20230926080752_cw721_and_cw20_add_smart_contract_address.ts
+++ migrations/20230926080752_cw721_and_cw20_add_smart_contract_address.ts
@@ -11,44 +11,39 @@ export async function up(knex: Knex): Promise<void> {
     .alterTable('cw20_contract', (table) => {
       table.string('smart_contract_address').index();
     });
-  await knex.transaction(async (trx) => {
-    const cw721Contracts = await CW721Contract.query()
-      .transacting(trx)
-      .withGraphFetched('smart_contract');
-    if (cw721Contracts.length > 0) {
-      cw721Contracts.forEach((cw721Contract) => {
-        cw721Contract.smart_contract_address =
-          cw721Contract.smart_contract.address;
-      });
-      await CW721Contract.query()
-        .transacting(trx)
-        .insert(
-          cw721Contracts.map((cw721Contract) =>
-            _.omit(cw721Contract, 'smart_contract')
-          )
+  const cw721Contracts = await CW721Contract.query(knex).withGraphFetched(
+    'smart_contract'
+  );
+  if (cw721Contracts.length > 0) {
+    cw721Contracts.forEach((cw721Contract) => {
+      cw721Contract.smart_contract_address =
+        cw721Contract.smart_contract.address;
+    });
+    await CW721Contract.query(knex)
+      .insert(
+        cw721Contracts.map((cw721Contract) =>
+          _.omit(cw721Contract, 'smart_contract')
         )
-        .onConflict('id')
-        .merge();
-    }
-    const cw20Contracts = await Cw20Contract.query()
-      .transacting(trx)
-      .withGraphFetched('smart_contract');
-    if (cw20Contracts.length > 0) {
-      cw20Contracts.forEach((cw20Contract) => {
-        cw20Contract.smart_contract_address =
-          cw20Contract.smart_contract.address;
-      });
-      await Cw20Contract.query()
-        .transacting(trx)
-        .insert(
-          cw20Contracts.map((cw20Contract) =>
-            _.omit(cw20Contract, 'smart_contract')
-          )
+      )
+      .onConflict('id')
+      .merge();
+  }
+  const cw20Contracts = await Cw20Contract.query(knex).withGraphFetched(
+    'smart_contract'
+  );
+  if (cw20Contracts.length > 0) {
+    cw20Contracts.forEach((cw20Contract) => {
+      cw20Contract.smart_contract_address = cw20Contract.smart_contract.address;
+    });
+    await Cw20Contract.query(knex)
+      .insert(
+        cw20Contracts.map((cw20Contract) =>
+          _.omit(cw20Contract, 'smart_contract')
         )
-        .onConflict('id')
-        .merge();
-    }
-  });
+      )
+      .onConflict('id')
+      .merge();
+  }
 }
 
 export async function down(knex: Knex): Promise<void> {
