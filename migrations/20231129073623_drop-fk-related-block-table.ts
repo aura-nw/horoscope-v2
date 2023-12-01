446250d4e2e903d08756a736bb95418786f58b02
matthew.nguyen.20.03.2023 <matthew.nguyen.20.03.2023@gmail.com>
2023-12-01 16:04:22 +0700

    feat: update sei

---
+++ migrations/20231129073623_drop-fk-related-block-table.ts
@@ -0,0 +1,27 @@
+import { Knex } from 'knex';
+
+// Drop block_height foreign key on transaction table that currently linked to block table
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('event', (table) => {
+    table.dropForeign('block_height', 'event_partition_block_foreign');
+  });
+  await knex.schema.alterTable('event_attribute', (table) => {
+    table.dropForeign(
+      'block_height',
+      'event_attribute_partition_block_height_foreign'
+    );
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('event', (table) => {
+    table
+      .foreign('block_height', 'event_partition_block_foreign')
+      .references('block.height');
+  });
+  await knex.schema.alterTable('event_attribute', (table) => {
+    table
+      .foreign('block_height', 'event_attribute_partition_block_height_foreign')
+      .references('block.height');
+  });
+}
