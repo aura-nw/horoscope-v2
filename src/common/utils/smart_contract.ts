e59ce801fb343f99b5b6a3887df14120dc88fc68
Vu Ngoc Quang <quang.vn@outlook.com>
2023-12-05 16:57:41 +0700

    fix: remove redundant join tx in smart contract event

--- src/common/utils/smart_contract.ts
+++ src/common/utils/smart_contract.ts
@@ -25,30 +25,25 @@ export async function getContractActivities(
   const contractActivities: IContractMsgInfo[] = [];
   const wasmEvents = await Event.query()
     .alias('event')
-    .withGraphJoined(
-      '[attributes(selectAttribute), transaction(filterSuccess,selectTransaction)]'
-    )
+    .withGraphFetched('[attributes(selectAttribute)]')
     .modifiers({
       selectAttribute(builder) {
-        builder.select('event_id', 'index', 'key', 'value');
+        builder.select('event_id', 'index', 'key', 'value').orderBy([
+          { column: 'attributes.event_id', order: 'ASC' },
+          { column: 'attributes.index', order: 'ASC' },
+        ]);
       },
-      selectTransaction(builder) {
-        builder.select('hash', 'height');
-      },
-      filterSuccess(builder) {
-        builder.where('code', 0);
-      },
+      // NOTE: wasm events emitted should mean succeeded transactions
+      // filterSuccess(builder) {
+      //   builder.where('code', 0);
+      // },
     })
     .whereIn('event.type', [
       Event.EVENT_TYPE.WASM,
       Event.EVENT_TYPE.INSTANTIATE,
     ])
     .where('event.block_height', '>', fromBlock)
-    .andWhere('event.block_height', '<=', toBlock)
-    .orderBy([
-      { column: 'attributes.event_id', order: 'ASC' },
-      { column: 'attributes.index', order: 'ASC' },
-    ]);
+    .andWhere('event.block_height', '<=', toBlock);
 
   wasmEvents.forEach((wasmEvent, index) => {
     const wasmAttribute: { key: string; value: string }[] =
