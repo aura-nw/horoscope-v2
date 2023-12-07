019db49671a89a349100a2eca08303502661cc24
matthew.nguyen.20.03.2023 <matthew.nguyen.20.03.2023@gmail.com>
2023-12-07 10:08:07 +0700

    feat: separate migration for development and staging environtment

--- migrations/20231019031400_drop_old_btree_index_transaction.ts
+++ migrations/20231019031400_drop_old_btree_index_transaction.ts
@@ -1,6 +1,10 @@
 import { Knex } from 'knex';
+import { environmentDeploy } from '../src/common';
+const envDeploy = process.env.NODE_ENV;
 
 export async function up(knex: Knex): Promise<void> {
+  if (envDeploy !== environmentDeploy.development) return;
+
   await knex.schema.alterTable('transaction', (table) => {
     table.dropIndex('height', 'transaction_height_index');
     table.dropIndex('timestamp', 'transaction_timestamp_index');
@@ -8,6 +12,8 @@ export async function up(knex: Knex): Promise<void> {
 }
 
 export async function down(knex: Knex): Promise<void> {
+  if (envDeploy !== environmentDeploy.development) return;
+
   await knex.schema.alterTable('transaction', (table) => {
     table.index('height', 'transaction_height_index', 'btree');
     table.index('timestamp', 'transaction_timestamp_index', 'btree');
--- migrations/20231019034210_create_brin_index_event_atttribute.ts
+++ migrations/20231019034210_create_brin_index_event_atttribute.ts
@@ -1,5 +1,10 @@
 import { Knex } from 'knex';
+import { environmentDeploy } from '../src/common';
+const envDeploy = process.env.NODE_ENV;
+
 export async function up(knex: Knex): Promise<void> {
+  if (envDeploy !== environmentDeploy.development) return;
+
   await knex.raw(`
     CREATE INDEX IF NOT EXISTS brin_idx_blh_event_attribute
     ON event_attribute USING brin (block_height) WITH (PAGES_PER_RANGE = 10, AUTOSUMMARIZE = true)
@@ -10,6 +15,8 @@ export async function up(knex: Knex): Promise<void> {
   });
 }
 export async function down(knex: Knex): Promise<void> {
+  if (envDeploy !== environmentDeploy.development) return;
+
   await knex.schema.alterTable('event_attribute', (table) => {
     table.dropIndex('block_height', `brin_idx_blh_event_attribute`);
     table.dropIndex('tx_id', `brin_idx_tx_id_event_attribute}`);
