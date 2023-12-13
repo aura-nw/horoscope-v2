388d2923c1837839a24afc2efbf7e107e5309846
Tuan Phan Anh <38557844+fibonacci998@users.noreply.github.com>
2023-12-13 10:57:52 +0700

    Fix/improve crawl delegator (staging) (#531)
    * fix: improve crawl delegator job
    
    * fix: remove unuse testcase
    
    * fix: remove unuse testcase
    
    ---------
    
    Co-authored-by: matthew.nguyen.20.03.2023 <matthew.nguyen.20.03.2023@gmail.com>

--- src/services/crawl-validator/crawl_delegators.service.ts
+++ src/services/crawl-validator/crawl_delegators.service.ts
@@ -1,20 +1,16 @@
 /* eslint-disable no-await-in-loop */
 import { Service } from '@ourparentcenter/moleculer-decorators-extended';
 import { ServiceBroker } from 'moleculer';
-import Long from 'long';
-import { fromBase64 } from '@cosmjs/encoding';
 import BullableService, { QueueHandler } from '../../base/bullable.service';
 import config from '../../../config.json' assert { type: 'json' };
 import {
   BULL_JOB_NAME,
   getLcdClient,
   IAuraJSClientFactory,
-  IPagination,
   IValidatorDelegators,
   SERVICE,
 } from '../../common';
-import { BlockCheckpoint, Delegator, Validator } from '../../models';
-import knex from '../../common/utils/db_connection';
+import { BlockCheckpoint, Validator } from '../../models';
 
 @Service({
   name: SERVICE.V1.CrawlDelegatorsService.key,
@@ -30,7 +26,6 @@ export default class CrawlDelegatorsService extends BullableService {
   @QueueHandler({
     queueName: BULL_JOB_NAME.CRAWL_DELEGATORS,
     jobName: BULL_JOB_NAME.CRAWL_DELEGATORS,
-    // prefix: `horoscope-v2-${config.chainId}`,
   })
   public async handleJob(_payload: object): Promise<void> {
     this.logger.info('Update validator delegators');
@@ -60,86 +55,36 @@ export default class CrawlDelegatorsService extends BullableService {
   @QueueHandler({
     queueName: BULL_JOB_NAME.CRAWL_VALIDATOR_DELEGATORS,
     jobName: BULL_JOB_NAME.CRAWL_VALIDATOR_DELEGATORS,
-    // prefix: `horoscope-v2-${config.chainId}`,
   })
   public async handleJobCrawlValidatorDelegators(
     _payload: IValidatorDelegators
   ): Promise<void> {
     this._lcdClient = await getLcdClient();
 
-    const delegations: any[] = [];
-    let delegators: Delegator[] = [];
-
-    let resultCallApi;
-    let done = false;
-    const pagination: IPagination = {
-      limit: Long.fromInt(config.crawlDelegators.queryPageLimit),
+    const pagination = {
+      limit: 1,
+      countTotal: true,
     };
+    const validatorDelegationInfo =
+      await this._lcdClient.auranw.cosmos.staking.v1beta1.validatorDelegations({
+        validatorAddr: _payload.address,
+        pagination,
+      });
 
-    while (!done) {
-      resultCallApi =
-        await this._lcdClient.auranw.cosmos.staking.v1beta1.validatorDelegations(
-          {
-            validatorAddr: _payload.address,
-            pagination,
-          }
-        );
-
-      delegations.push(...resultCallApi.delegation_responses);
-      if (resultCallApi.pagination.next_key === null) {
-        done = true;
-      } else {
-        pagination.key = fromBase64(resultCallApi.pagination.next_key);
-      }
-    }
-
-    if (delegations.length > 0) {
-      delegators = delegations.map((delegate) =>
-        Delegator.fromJson({
-          validator_id: _payload.id,
-          delegator_address: delegate.delegation.delegator_address,
-          amount: delegate.balance.amount,
-        })
-      );
-    }
-
+    const totalDelegator = Number(validatorDelegationInfo.pagination.total);
     const latestBlock: BlockCheckpoint | undefined =
       await BlockCheckpoint.query()
         .where('job_name', BULL_JOB_NAME.CRAWL_BLOCK)
         .first();
 
-    await knex.transaction(async (trx) => {
-      await Promise.all([
-        Delegator.query()
-          .insert(delegators)
-          .onConflict(['validator_id', 'delegator_address'])
-          .merge()
-          .transacting(trx)
-          .catch((error) => {
-            this.logger.error(
-              `Insert or update validator delegators error: ${_payload.address}`
-            );
-            this.logger.error(error);
-          }),
-        Delegator.query()
-          .delete(true)
-          .whereNotIn(
-            'delegator_address',
-            delegators.map((delegate) => delegate.delegator_address)
-          )
-          .andWhere('validator_id', _payload.id)
-          .transacting(trx),
-        Validator.query()
-          .patch({
-            delegators_count: delegations.length,
-            delegators_last_height: latestBlock
-              ? latestBlock.height
-              : _payload.height,
-          })
-          .where('id', _payload.id)
-          .transacting(trx),
-      ]);
-    });
+    await Validator.query()
+      .patch({
+        delegators_count: totalDelegator,
+        delegators_last_height: latestBlock
+          ? latestBlock.height
+          : _payload.height,
+      })
+      .where('id', _payload.id);
   }
 
   public async _start() {
