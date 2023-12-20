146d7816c96721f7a738741a91cff1632bbd139d
Phan Anh Tuan <fibonacci998@gmail.com>
2023-12-20 13:51:49 +0700

    fix: remove filter resultEvents has validator or source_validator

--- src/services/crawl-validator/handle_stake_event.service.ts
+++ src/services/crawl-validator/handle_stake_event.service.ts
@@ -63,104 +63,93 @@ export default class HandleStakeEventService extends BullableService {
     const validatorKeys = _.keyBy(validators, 'operator_address');
 
     const powerEvents: PowerEvent[] = [];
-    resultEvents
-      .filter(
-        (event) =>
-          this.eventStakes.includes(event.type) &&
-          (event.attributes
-            .map((a: any) => a.key)
-            .includes(EventAttribute.ATTRIBUTE_KEY.VALIDATOR) ||
-            event.attributes
-              .map((a: any) => a.key)
-              .includes(EventAttribute.ATTRIBUTE_KEY.SOURCE_VALIDATOR))
-      )
-      .forEach((stakeEvent) => {
-        try {
-          let validatorSrcId;
-          let validatorDstId;
-          let amount;
-          let amountRaw;
-          const firstValidator = stakeEvent.attributes.find(
-            (attr: any) =>
-              attr.key === EventAttribute.ATTRIBUTE_KEY.VALIDATOR ||
-              attr.key === EventAttribute.ATTRIBUTE_KEY.SOURCE_VALIDATOR
+    resultEvents.forEach((stakeEvent) => {
+      try {
+        let validatorSrcId;
+        let validatorDstId;
+        let amount;
+        let amountRaw;
+        const firstValidator = stakeEvent.attributes.find(
+          (attr: any) =>
+            attr.key === EventAttribute.ATTRIBUTE_KEY.VALIDATOR ||
+            attr.key === EventAttribute.ATTRIBUTE_KEY.SOURCE_VALIDATOR
+        );
+        if (!firstValidator) {
+          this.logger.warn(
+            `stake event ${stakeEvent.id} doesn't have first validator`
           );
-          if (!firstValidator) {
-            this.logger.warn(
-              `stake event ${stakeEvent.id} doesn't have first validator`
-            );
-            return;
-          }
-          const destValidator = stakeEvent.attributes.find(
-            (attr: any) =>
-              attr.key === EventAttribute.ATTRIBUTE_KEY.DESTINATION_VALIDATOR
-          );
-          switch (stakeEvent.type) {
-            case PowerEvent.TYPES.DELEGATE: {
-              validatorDstId = validatorKeys[firstValidator.value].id;
-              break;
-            }
-            case PowerEvent.TYPES.REDELEGATE:
-              validatorSrcId = validatorKeys[firstValidator.value].id;
-              if (!destValidator) {
-                this.logger.warn(
-                  `stake event ${stakeEvent.id} doesn't have destination validator`
-                );
-                return;
-              }
-              validatorDstId = validatorKeys[destValidator.value].id;
-              amountRaw = stakeEvent.attributes.find(
-                (attr: any) =>
-                  attr.key === EventAttribute.ATTRIBUTE_KEY.AMOUNT &&
-                  attr.index === firstValidator.index + 2
-              );
-              amount = amountRaw?.value
-                ? parseCoins(amountRaw?.value)[0].amount
-                : '0';
-              break;
-            case PowerEvent.TYPES.UNBOND:
-              validatorSrcId = validatorKeys[firstValidator.value].id;
-              break;
-            case PowerEvent.TYPES.CREATE_VALIDATOR:
-              validatorDstId = validatorKeys[firstValidator.value].id;
-              amountRaw = stakeEvent.attributes.find(
-                (attr: any) =>
-                  attr.key === EventAttribute.ATTRIBUTE_KEY.AMOUNT &&
-                  attr.index === firstValidator.index + 1
-              );
-              amount = amountRaw?.value
-                ? parseCoins(amountRaw?.value)[0].amount
-                : '0';
-              break;
-            default:
-              break;
-          }
-          amountRaw = stakeEvent.attributes.find(
-            (attr: any) =>
-              attr.key === EventAttribute.ATTRIBUTE_KEY.AMOUNT &&
-              attr.index === firstValidator.index + 1
-          );
-          if (amountRaw) {
-            amount = amount ?? parseCoins(amountRaw?.value)[0].amount;
-          }
-          const powerEvent: PowerEvent = PowerEvent.fromJson({
-            tx_id: stakeEvent.transaction.id,
-            height: stakeEvent.block_height,
-            type: stakeEvent.type,
-            validator_src_id: validatorSrcId,
-            validator_dst_id: validatorDstId,
-            amount,
-            time: stakeEvent.transaction.timestamp.toISOString(),
-          });
-
-          powerEvents.push(powerEvent);
-        } catch (error) {
-          this.logger.error(
-            `Error create power event: ${JSON.stringify(stakeEvent)}`
-          );
-          this.logger.error(error);
+          return;
         }
-      });
+        const destValidator = stakeEvent.attributes.find(
+          (attr: any) =>
+            attr.key === EventAttribute.ATTRIBUTE_KEY.DESTINATION_VALIDATOR
+        );
+        switch (stakeEvent.type) {
+          case PowerEvent.TYPES.DELEGATE: {
+            validatorDstId = validatorKeys[firstValidator.value].id;
+            break;
+          }
+          case PowerEvent.TYPES.REDELEGATE:
+            validatorSrcId = validatorKeys[firstValidator.value].id;
+            if (!destValidator) {
+              this.logger.warn(
+                `stake event ${stakeEvent.id} doesn't have destination validator`
+              );
+              return;
+            }
+            validatorDstId = validatorKeys[destValidator.value].id;
+            amountRaw = stakeEvent.attributes.find(
+              (attr: any) =>
+                attr.key === EventAttribute.ATTRIBUTE_KEY.AMOUNT &&
+                attr.index === firstValidator.index + 2
+            );
+            amount = amountRaw?.value
+              ? parseCoins(amountRaw?.value)[0].amount
+              : '0';
+            break;
+          case PowerEvent.TYPES.UNBOND:
+            validatorSrcId = validatorKeys[firstValidator.value].id;
+            break;
+          case PowerEvent.TYPES.CREATE_VALIDATOR:
+            validatorDstId = validatorKeys[firstValidator.value].id;
+            amountRaw = stakeEvent.attributes.find(
+              (attr: any) =>
+                attr.key === EventAttribute.ATTRIBUTE_KEY.AMOUNT &&
+                attr.index === firstValidator.index + 1
+            );
+            amount = amountRaw?.value
+              ? parseCoins(amountRaw?.value)[0].amount
+              : '0';
+            break;
+          default:
+            break;
+        }
+        amountRaw = stakeEvent.attributes.find(
+          (attr: any) =>
+            attr.key === EventAttribute.ATTRIBUTE_KEY.AMOUNT &&
+            attr.index === firstValidator.index + 1
+        );
+        if (amountRaw) {
+          amount = amount ?? parseCoins(amountRaw?.value)[0].amount;
+        }
+        const powerEvent: PowerEvent = PowerEvent.fromJson({
+          tx_id: stakeEvent.transaction.id,
+          height: stakeEvent.block_height,
+          type: stakeEvent.type,
+          validator_src_id: validatorSrcId,
+          validator_dst_id: validatorDstId,
+          amount,
+          time: stakeEvent.transaction.timestamp.toISOString(),
+        });
+
+        powerEvents.push(powerEvent);
+      } catch (error) {
+        this.logger.error(
+          `Error create power event: ${JSON.stringify(stakeEvent)}`
+        );
+        this.logger.error(error);
+      }
+    });
 
     await knex.transaction(async (trx) => {
       if (powerEvents.length > 0)
