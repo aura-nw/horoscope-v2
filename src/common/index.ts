c59a267315e844b046f8348ffd6765162c02c6a0
matthew.nguyen.20.03.2023 <matthew.nguyen.20.03.2023@gmail.com>
2023-12-20 15:23:20 +0700

    feat: switch registry by config chain id

--- src/common/index.ts
+++ src/common/index.ts
@@ -6,6 +6,7 @@ export { config as Config };
 export * from './constant';
 export * from './utils/aurajs_client';
 export * from './utils/cosmjs_client';
+// eslint-disable-next-line import/no-cycle
 export * from './utils/helper';
 export * from './utils/request';
 export * from './types/interfaces';
