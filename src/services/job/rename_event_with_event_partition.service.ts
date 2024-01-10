aa0be46a6956cd86cfb11b1edfc43cecea20db58
matthew.nguyen.20.03.2023 <matthew.nguyen.20.03.2023@gmail.com>
2024-01-10 14:57:28 +0700

    fix: fix comment

--- src/services/job/rename_event_with_event_partition.service.ts
+++ src/services/job/rename_event_with_event_partition.service.ts
@@ -2,9 +2,10 @@
 import { Service } from '@ourparentcenter/moleculer-decorators-extended';
 import { ServiceBroker } from 'moleculer';
 import BullableService, { QueueHandler } from '../../base/bullable.service';
-import { BULL_JOB_NAME, SERVICE, sleep } from '../../common';
+import { BULL_JOB_NAME, SERVICE } from '../../common';
 import knex from '../../common/utils/db_connection';
 import { BlockCheckpoint, Event } from '../../models';
+import config from '../../../config.json' assert { type: 'json' };
 
 @Service({
   name: SERVICE.V1.JobService.RenameEventPartitionToEvent.key,
@@ -17,55 +18,64 @@ export default class RenameEventWithEventPartitionJob extends BullableService {
 
   public async switchEventPartitionToEvent(): Promise<void> {
     const trx = await knex.transaction();
-    // Lock both table
-    await trx.raw('LOCK TABLE event, event_partition');
-    // Drop FK related
-    await trx.raw(`
+    try {
+      // Lock both table
+      await trx.raw('LOCK TABLE event, event_partition');
+      // Drop FK related
+      await trx.raw(`
         ALTER TABLE event_attribute DROP CONSTRAINT IF EXISTS event_attribute_partition_event_id_foreign cascade;
         ALTER TABLE smart_contract_event DROP CONSTRAINT IF EXISTS smart_contract_event_event_id_foreign cascade;
       `);
-    // Switch two table
-    await trx.raw('ALTER TABLE event RENAME TO event_backup;');
-    await trx.raw('ALTER TABLE event_partition RENAME TO event;');
+      // Switch two table
+      await trx.raw('ALTER TABLE event RENAME TO event_backup;');
+      await trx.raw('ALTER TABLE event_partition RENAME TO event;');
 
-    // update id seq for event_partition
-    const currentEventIdSeq = await trx.raw(
-      'SELECT last_value FROM transaction_event_id_seq;'
-    );
-    const lastEventId = currentEventIdSeq.rows[0].last_value;
-    await trx.raw(
-      `SELECT setval('event_partition_id_seq', ${lastEventId}, true);`
-    );
-    await trx.commit();
+      // update id seq for event_partition
+      const currentEventIdSeq = await trx.raw(
+        'SELECT last_value FROM transaction_event_id_seq;'
+      );
+      const lastEventId = currentEventIdSeq.rows[0].last_value;
+      await trx.raw(
+        `SELECT setval('event_partition_id_seq', ${lastEventId}, true);`
+      );
+      await trx.commit();
+    } catch (error) {
+      trx.rollback();
+      this.logger.error(error);
+    }
   }
 
   @QueueHandler({
     queueName: BULL_JOB_NAME.JOB_RENAME_EVENT_PARTITION,
     jobName: BULL_JOB_NAME.JOB_RENAME_EVENT_PARTITION,
   })
-  public async migrateEventPartition(): Promise<boolean> {
-    let start = false;
-    while (!start) {
-      const latestEvent = await Event.query().orderBy('id', 'DESC').limit(1);
-      const migrateEventCheckPoint = await BlockCheckpoint.query()
-        .where('job_name', BULL_JOB_NAME.CP_MIGRATE_DATA_EVENT_TABLE)
-        .first();
+  public async migrateEventPartition(): Promise<void> {
+    const eventPartitionCount = await knex.raw(`
+      SELECT count(*) AS partition_count
+      FROM pg_catalog.pg_inherits
+      WHERE inhparent = 'event'::regclass;
+    `);
+    if (eventPartitionCount.rows[0].partition_count > 0) {
+      this.logger.info('Event already partitioned');
+      return;
+    }
 
-      if (
-        latestEvent[0].id.toString() !==
-        migrateEventCheckPoint?.height.toString()
-      ) {
-        this.logger.info(`
-          Waiting for event migration done, current event ${latestEvent[0].id}, current migration ${migrateEventCheckPoint?.height}
-        `);
-        await sleep(3000);
-      } else {
-        start = true;
-      }
+    const latestEvent = await Event.query().orderBy('id', 'DESC').limit(1);
+    const migrateEventCheckPoint = await BlockCheckpoint.query()
+      .where('job_name', BULL_JOB_NAME.CP_MIGRATE_DATA_EVENT_TABLE)
+      .first();
+
+    if (
+      latestEvent[0].id.toString() !== migrateEventCheckPoint?.height.toString()
+    ) {
+      this.logger.info(`
+        Waiting for event migration done, current event ${latestEvent[0].id}, current migration ${migrateEventCheckPoint?.height}
+      `);
+      return;
     }
 
     await this.switchEventPartitionToEvent();
-    return true;
+    this.logger.info('Successfully switch table event');
   }
 
   public async _start(): Promise<void> {
@@ -77,6 +87,9 @@ export default class RenameEventWithEventPartitionJob extends BullableService {
         removeOnComplete: true,
         removeOnFail: {
           count: 3,
+        },
+        repeat: {
+          every: config.jobRenameEventTable.millisecondRepeat,
         },
       }
     );
