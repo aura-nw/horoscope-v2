12828ec0608c7ca90bdc14745695d80473c12cc6
matthew-nguyen-20032023 <129312342+matthew-nguyen-20032023@users.noreply.github.com>
2024-01-16 14:08:14 +0700

    Sei/cherry pick block partition, update hasura (#598)
    * fix: fix aurajs client
    
    * fix: fix aurajs client
    
    * fix: fix aurajs client
    
    * fix: fix aurajs client
    
    * fix: fix aurajs client
    
    * feat: partition for block table
    
    * fix: fix unit test for block partition
    
    * fix: fix unit test for block partition
    
    * fix: update hasura
    
    * fix: fix migration block partition for sei

--- test/unit/services/crawl-account/handle_address.spec.ts
+++ test/unit/services/crawl-account/handle_address.spec.ts
@@ -141,7 +141,9 @@ export default class HandleAddressTest {
     await Promise.all([
       Account.query().delete(true),
       BlockCheckpoint.query().delete(true),
-      knex.raw('TRUNCATE TABLE block, transaction RESTART IDENTITY CASCADE'),
+      knex.raw(
+        'TRUNCATE TABLE block, block_signature, transaction, event, event_attribute RESTART IDENTITY CASCADE'
+      ),
     ]);
     await Block.query().insert(this.blocks);
     await Transaction.query().insertGraph(this.txInsert);
@@ -153,7 +155,9 @@ export default class HandleAddressTest {
     await Promise.all([
       Account.query().delete(true),
       BlockCheckpoint.query().delete(true),
-      knex.raw('TRUNCATE TABLE block, transaction RESTART IDENTITY CASCADE'),
+      knex.raw(
+        'TRUNCATE TABLE block, block_signature, transaction, event, event_attribute RESTART IDENTITY CASCADE'
+      ),
     ]);
     await this.broker.stop();
   }
