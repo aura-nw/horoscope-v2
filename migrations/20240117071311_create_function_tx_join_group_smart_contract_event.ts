08e25ad8881d98bf21f526095bd1e77e5e788e55
fibonacci998 <fibonacci998@gmail.com>
2024-01-17 14:15:10 +0700

    feat: create function tx join group smart contract event

---
+++ migrations/20240117071311_create_function_tx_join_group_smart_contract_event.ts
@@ -0,0 +1,22 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.raw(`
+    CREATE OR REPLACE FUNCTION public.transaction_join_group_smart_contract_event(contractaddress character varying)
+    RETURNS SETOF transaction
+    LANGUAGE sql
+    STABLE PARALLEL SAFE
+    AS $function$
+          select transaction.* from transaction right join (
+            select tx_id as txid from smart_contract_event
+            join smart_contract on smart_contract_event.smart_contract_id = smart_contract.id
+            where smart_contract.address = contractaddress
+            group by tx_id order by tx_id
+          ) a on a.txid = transaction.id
+    $function$`);
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.raw(`
+    DROP FUNCTION IF EXISTS public.transaction_join_group_smart_contract_event(contractaddress character varying)`);
+}
