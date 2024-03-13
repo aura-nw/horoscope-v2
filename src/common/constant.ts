93a360839c00e35ad84c4528ec7e93ae2b987975
matthew-nguyen-20032023 <129312342+matthew-nguyen-20032023@users.noreply.github.com>
2024-03-13 09:45:16 +0700

    Feat/evm contract topic mapping (#686) TG-192 #ready_to_test
    * feat: signature mapping
    
    * feat: signature mapping
    
    * feat: signature mapping
    
    * feat: fix comment

--- src/common/constant.ts
+++ src/common/constant.ts
@@ -132,6 +132,7 @@ export const BULL_JOB_NAME = {
     'job:create-evm-transaction-constraint',
   JOB_UPDATE_TX_COUNT_IN_BLOCK: 'job:update-tx-count-in-block',
   HANDLE_TRANSACTION_EVM: 'handle:transaction-evm',
+  HANDLE_EVM_SIGNATURE_MAPPING: 'handle:evm-signature-mapping',
   CRAWL_SMART_CONTRACT_EVM: 'crawl:smart-contract-evm',
   VERIFY_CONTRACT_EVM: 'verify:contract-evm',
 };
@@ -461,6 +462,14 @@ export const SERVICE = {
       inputRequestVerify: {
         key: 'inputRequestVerify',
         path: 'v1.VerifyContractEVM.inputRequestVerify',
+      },
+    },
+    SignatureMappingEVM: {
+      key: 'SignatureMappingEVM',
+      path: 'v1.SignatureMappingEVM',
+      action: {
+        key: 'createJobMapping',
+        path: 'v1.SignatureMappingEVM.createJobMapping',
       },
     },
   },
