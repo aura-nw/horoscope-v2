e1fecbfe3dbaa00f707b18c033f22df88841701d
phamphong9981 <phamphong9981@gmail.com>
2024-03-14 09:42:21 +0700

    refactor: migration

--- migrations/20240313025300_erc20_contract_init.ts
+++ migrations/20240313025300_erc20_contract_init.ts
@@ -4,10 +4,10 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('erc20_contract', (table) => {
     table.increments();
     table.integer('evm_smart_contract_id').notNullable().unique();
-    table.string('symbol').index().notNullable();
-    table.decimal('total_supply', 80, 0).notNullable();
+    table.string('symbol').index();
+    table.decimal('total_supply', 80, 0);
     table.string('decimal').index();
-    table.string('name').notNullable();
+    table.string('name');
     table.boolean('track').defaultTo(false).index();
     table.integer('last_updated_height').index();
     table.foreign('evm_smart_contract_id').references('evm_smart_contract.id');
