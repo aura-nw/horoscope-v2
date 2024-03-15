421525c2c0bfd029e99f48c2b219f3bd7c900c45
phamphong9981 <phamphong9981@gmail.com>
2024-03-15 10:59:48 +0700

    feat: crawl erc20 contract

--- fetch-polyfill.js
+++ fetch-polyfill.js
@@ -1,20 +1,20 @@
 // fetch-polyfill.js
 import fetch, {
-    Blob,
-    blobFrom,
-    blobFromSync,
-    File,
-    fileFrom,
-    fileFromSync,
-    FormData,
-    Headers,
-    Request,
-    Response,
-  } from 'node-fetch'
-  
-  if (!globalThis.fetch) {
-    globalThis.fetch = fetch
-    globalThis.Headers = Headers
-    globalThis.Request = Request
-    globalThis.Response = Response
-  }
+  Blob,
+  blobFrom,
+  blobFromSync,
+  File,
+  fileFrom,
+  fileFromSync,
+  FormData,
+  Headers,
+  Request,
+  Response,
+} from 'node-fetch';
+
+if (!globalThis.fetch) {
+  globalThis.fetch = fetch;
+  globalThis.Headers = Headers;
+  globalThis.Request = Request;
+  globalThis.Response = Response;
+}
