50ef764532dfb3a34bcddf44969e0d7341702fcc
phamphong9981 <phamphong9981@gmail.com>
2024-03-15 10:58:56 +0700

    feat: crawl erc20 contract

--- src/services/erc20/erc20.service.ts
+++ src/services/erc20/erc20.service.ts
@@ -1,20 +1,21 @@
 import { Service } from '@ourparentcenter/moleculer-decorators-extended';
-import { Interface, ethers, keccak256, toUtf8Bytes } from 'ethers';
 import { ServiceBroker } from 'moleculer';
-import { BlockCheckpoint, EVMSmartContract } from 'src/models';
+import { PublicClient, getContract } from 'viem';
 import config from '../../../config.json' assert { type: 'json' };
+import '../../../fetch-polyfill.js';
 import BullableService, { QueueHandler } from '../../base/bullable.service';
 import { BULL_JOB_NAME, SERVICE } from '../../common';
 import knex from '../../common/utils/db_connection';
 import EtherJsClient from '../../common/utils/etherjs_client';
+import { BlockCheckpoint, EVMSmartContract } from '../../models';
 import { Erc20Contract } from '../../models/erc20_contract';
 
 @Service({
   name: SERVICE.V1.Erc20.key,
   version: 1,
 })
-export default class Erc20HandlerService extends BullableService {
-  etherJsClient!: EtherJsClient;
+export default class Erc20Service extends BullableService {
+  etherJsClient!: PublicClient;
 
   public constructor(public broker: ServiceBroker) {
     super(broker);
@@ -38,15 +39,13 @@ export default class Erc20HandlerService extends BullableService {
         .where('created_height', '>', startBlock)
         .andWhere('created_height', '<=', endBlock)
         .andWhere('type', 'ERC20')
-        .limit(config.erc20.limitEvmSmartContractGet)
         .orderBy('id', 'asc');
       if (erc20SmartContracts.length > 0) {
         const erc20Instances = await this.getErc20Instances(
           erc20SmartContracts
         );
         await Erc20Contract.query().transacting(trx).insert(erc20Instances);
-        updateBlockCheckpoint.height =
-          erc20SmartContracts[erc20SmartContracts.length - 1].created_height;
+        updateBlockCheckpoint.height = endBlock;
         await BlockCheckpoint.query()
           .insert(updateBlockCheckpoint)
           .onConflict('job_name')
@@ -58,54 +57,51 @@ export default class Erc20HandlerService extends BullableService {
 
   async getErc20Instances(evmSmartContracts: EVMSmartContract[]) {
     const addresses = evmSmartContracts.map((e) => e.address);
-    const names = await this.getBatchErc20Info(addresses, 'name');
-    const totalSupplies = await this.getBatchErc20Info(
-      addresses,
-      'totalSupply'
+    const erc20ContractsInfo = await this.getBatchErc20Info(
+      addresses as `0x${string}`[]
     );
-    const decimals = await this.getBatchErc20Info(addresses, 'decimals');
-    const symbols = await this.getBatchErc20Info(addresses, 'symbol');
     return evmSmartContracts.map((e, index) =>
       Erc20Contract.fromJson({
-        evm_smart_contract_id: evmSmartContracts[index],
-        total_supply: totalSupplies[index],
-        symbol: symbols[index],
-        decimals: decimals[index],
-        name: names[index],
+        evm_smart_contract_id: evmSmartContracts[index].id,
+        total_supply: erc20ContractsInfo[index].totalSupply,
+        symbol: erc20ContractsInfo[index].symbol,
+        decimal: erc20ContractsInfo[index].decimals,
+        name: erc20ContractsInfo[index].name,
         track: true,
         last_updated_height: e.created_height,
       })
     );
   }
 
-  async getBatchErc20Info(addresses: string[], funcName: string) {
-    const jsonRpcProvider = this.etherJsClient
-      .etherJsClient as ethers.JsonRpcProvider;
-    const results = await jsonRpcProvider._send(
-      addresses.map((addr, index, _) => ({
-        id: index,
-        jsonrpc: '2.0',
-        method: 'eth_call',
-        params: [
-          {
-            data: keccak256(toUtf8Bytes(`${funcName}()`)),
-            to: addr,
-          },
-          'latest',
-        ],
-      }))
+  async getBatchErc20Info(addresses: `0x${string}`[]) {
+    const viemClient = EtherJsClient.getViemClient();
+    const contracts = addresses.map((address) =>
+      getContract({
+        address,
+        abi: Erc20Contract.ABI,
+        client: viemClient,
+      })
     );
-    return results.map(
-      (result) =>
-        new Interface(Erc20Contract.ABI).decodeFunctionResult(
-          funcName,
-          result.result
-        )[0]
-    );
+    const batchReqs: any[] = [];
+    contracts.forEach((e) => {
+      batchReqs.push(
+        e.read.name(),
+        e.read.symbol(),
+        e.read.decimals(),
+        e.read.totalSupply()
+      );
+    });
+    const results = await Promise.all(batchReqs);
+    return addresses.map((address, index) => ({
+      address,
+      name: results[4 * index],
+      symbol: results[4 * index + 1],
+      decimals: results[4 * index + 2],
+      totalSupply: results[4 * index + 3].toString(),
+    }));
   }
 
   public async _start(): Promise<void> {
-    this.etherJsClient = new EtherJsClient(true);
     await this.createJob(
       BULL_JOB_NAME.HANDLE_ERC20_CONTRACT,
       BULL_JOB_NAME.HANDLE_ERC20_CONTRACT,
