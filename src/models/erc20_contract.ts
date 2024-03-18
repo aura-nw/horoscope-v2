deafe52d1477a1103ffe0ff57a447ca1bf5a3a2d
Phạm Thành Phong <49814372+phamphong9981@users.noreply.github.com>
2024-03-18 10:43:10 +0700

    Feat/crawl erc20 contract (#696) TG-197 #ready_to_test
    * feat: handle erc20 contract
    
    * refactor: lint
    
    * refactor: get by block height
    
    * refactor: migration
    
    * refactor: code
    
    * refactor: code
    
    * refactor: upgrade typescript from 4.8.4 to 5.4.2
    
    * feat: crawl erc20 contract
    
    * feat: crawl erc20 contract
    
    * refactor: lint
    
    * refactor: lint
    
    * refactor: review
    
    * fix: lint
    
    * fix: refactor
    
    * fix: refactor
    
    * fix: refactor
    
    * fix: refactor
    
    * fix: refactor
    
    * fix: refactor

---
+++ src/models/erc20_contract.ts
@@ -0,0 +1,115 @@
+import { Model } from 'objection';
+import BaseModel from './base';
+import { EVMSmartContract } from './evm_smart_contract';
+
+export class Erc20Contract extends BaseModel {
+  static softDelete = false;
+
+  id!: number;
+
+  evm_smart_contract_id!: number;
+
+  total_supply!: string;
+
+  symbol?: string;
+
+  address!: string;
+
+  decimals?: string;
+
+  name?: string;
+
+  track!: boolean;
+
+  last_updated_height!: number;
+
+  static get tableName() {
+    return 'erc20_contract';
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['evm_smart_contract_id'],
+      properties: {
+        evm_smart_contract_id: { type: 'number' },
+        total_supply: { type: 'string' },
+        symbol: { type: 'string' },
+        decimal: { type: 'string' },
+        name: { type: 'string' },
+      },
+    };
+  }
+
+  static get relationMappings() {
+    return {
+      evm_smart_contract: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: EVMSmartContract,
+        join: {
+          to: 'evm_smart_contract.id',
+          from: 'erc20_contract.evm_smart_contract_id',
+        },
+      },
+    };
+  }
+
+  static ABI = [
+    {
+      constant: true,
+      inputs: [],
+      name: 'name',
+      outputs: [
+        {
+          name: '',
+          type: 'string',
+        },
+      ],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: 'totalSupply',
+      outputs: [
+        {
+          name: '',
+          type: 'uint256',
+        },
+      ],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: 'decimals',
+      outputs: [
+        {
+          name: '',
+          type: 'uint8',
+        },
+      ],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: 'symbol',
+      outputs: [
+        {
+          name: '',
+          type: 'string',
+        },
+      ],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function',
+    },
+  ];
+}
