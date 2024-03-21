ba2a9ed625444ed0d05467451359d7e885c1162d
fibonacci998 <fibonacci998@gmail.com>
2024-03-21 08:25:59 +0700

    feat: remove burned on index cw721_token_owner_not_burned

--- migrations/20240320074436_create_index_owner_not_burned_cw721_token.ts
+++ migrations/20240320074436_create_index_owner_not_burned_cw721_token.ts
@@ -2,7 +2,7 @@ import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.raw(
-    `CREATE INDEX IF NOT EXISTS cw721_token_owner_not_burned ON cw721_token USING btree (owner ASC NULLS LAST, burned ASC NULLS LAST) WHERE NOT burned;`
+    `CREATE INDEX IF NOT EXISTS cw721_token_owner_not_burned ON cw721_token USING btree (owner ASC NULLS LAST) WHERE NOT burned;`
   );
 }
 
