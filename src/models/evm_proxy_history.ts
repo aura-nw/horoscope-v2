78831da874277ce0a33d452d5dc34a2ee8fddc85
hoangtv49 <92872128+hoangtv49@users.noreply.github.com>
2024-03-26 10:35:35 +0700

    feat: evm proxy contract migration (#723)
    * feat: evm proxy contract migration
    
    * feat: evm proxy contract migration: change name of column

---
+++ src/models/evm_proxy_history.ts
@@ -0,0 +1,25 @@
+import BaseModel from './base';
+
+export class EvmProxyHistory extends BaseModel {
+  id!: number;
+
+  proxy_contract!: string;
+
+  implementation_contract!: string;
+
+  admin!: string;
+
+  tx_hash!: number;
+
+  block_height!: number;
+
+  last_updated_height!: number;
+
+  created_at!: Date;
+
+  updated_at!: Date;
+
+  static get tableName() {
+    return 'evm_proxy_history';
+  }
+}
