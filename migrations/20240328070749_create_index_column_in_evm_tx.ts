5101ed409002dc9ee906b7c0c95d4801bdb22c66
fibonacci998 <fibonacci998@gmail.com>
2024-03-28 14:54:12 +0700

    feat: create composite index (height, index) in evm_tx

--- migrations/20240328070749_create_index_column_in_evm_tx.ts
+++ migrations/20240328070749_create_index_column_in_evm_tx.ts
@@ -2,12 +2,14 @@ import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable('evm_transaction', (table) => {
-    table.integer('index').index();
+    table.integer('index');
+    table.index(['height', 'index']);
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.alterTable('evm_transaction', (table) => {
+    table.dropIndex(['height', 'index']);
     table.dropColumn('index');
   });
 }
