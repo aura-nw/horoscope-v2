93759a7afdb4696adb428ab73b017f063d7db254
phamphong9981 <phamphong9981@gmail.com>
2024-03-28 14:32:37 +0700

    refactor: review

--- migrations/20240328035728_erc20_activity_add_evm_tx_id.ts
+++ migrations/20240328035728_erc20_activity_add_evm_tx_id.ts
@@ -6,19 +6,16 @@ export async function up(knex: Knex): Promise<void> {
     table.integer('evm_transaction_id').index();
     table.foreign('evm_transaction_id').references('evm_transaction.id');
   });
-  await knex.transaction(async (trx) => {
-    const erc20Activities = await Erc20Activity.query()
-      .transacting(trx)
-      .join('evm_transaction', 'erc20_activity.tx_hash', 'evm_transaction.hash')
-      .select('erc20_activity.*', 'evm_transaction.id as evm_transaction_id');
-    if (erc20Activities.length > 0) {
-      await Erc20Activity.query()
-        .transacting(trx)
-        .insert(erc20Activities)
-        .onConflict(['id'])
-        .merge();
-    }
-  });
+  const erc20Activities = await Erc20Activity.query(knex)
+    .join('evm_transaction', 'erc20_activity.tx_hash', 'evm_transaction.hash')
+    .select('erc20_activity.*', 'evm_transaction.id as evm_transaction_id');
+  const stringListUpdates = erc20Activities.map(
+    (erc20Activity) =>
+      `(${erc20Activity.id}, ${erc20Activity.evm_transaction_id})`
+  );
+  await knex.raw(
+    `UPDATE erc20_activity SET evm_transaction_id = temp.evm_transaction_id from (VALUES ${stringListUpdates}) as temp(id, evm_transaction_id) where temp.id = erc20_activity.id`
+  );
 }
 
 export async function down(knex: Knex): Promise<void> {
