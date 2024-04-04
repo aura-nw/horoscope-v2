eb0d7fa14193cb83d79680625d05db22dc74ba07
Phạm Thành Phong <49814372+phamphong9981@users.noreply.github.com>
2024-04-04 09:21:59 +0700

    Feat/crawl erc721 contract (#704) TG-203 #ready-to-test
    * feat: crawl erc721 contract
    
    * refactor: code
    
    * refactor: code
    
    * fix: merge code
    
    * refactor: code
    
    * refactor: constant
    
    * fix: migration dỉ

---
+++ src/models/erc721_contract.ts
@@ -0,0 +1,212 @@
+import { Model } from 'objection';
+import BaseModel from './base';
+import { EVMSmartContract } from './evm_smart_contract';
+
+export class Erc721Contract extends BaseModel {
+  static softDelete = false;
+
+  id!: number;
+
+  evm_smart_contract_id!: number;
+
+  address!: string;
+
+  symbol?: string;
+
+  name?: string;
+
+  track!: boolean;
+
+  last_updated_height!: number;
+
+  static get tableName() {
+    return 'erc721_contract';
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['evm_smart_contract_id'],
+      properties: {
+        evm_smart_contract_id: { type: 'number' },
+        symbol: { type: 'string' },
+        name: { type: 'string' },
+      },
+    };
+  }
+
+  static get relationMappings() {
+    return {
+      evm_smart_contract: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: EVMSmartContract,
+        join: {
+          to: 'evm_smart_contract.id',
+          from: 'erc721_contract.evm_smart_contract_id',
+        },
+      },
+    };
+  }
+
+  static ABI = [
+    {
+      constant: true,
+      inputs: [],
+      name: 'totalSupply',
+      outputs: [
+        {
+          name: '',
+          type: 'uint256',
+        },
+      ],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: 'owner',
+          type: 'address',
+        },
+      ],
+      name: 'balanceOf',
+      outputs: [
+        {
+          internalType: 'uint256',
+          name: 'balance',
+          type: 'uint256',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'uint256',
+          name: 'tokenId',
+          type: 'uint256',
+        },
+      ],
+      name: 'getApproved',
+      outputs: [
+        {
+          internalType: 'address',
+          name: 'operator',
+          type: 'address',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: 'owner',
+          type: 'address',
+        },
+        {
+          internalType: 'address',
+          name: 'operator',
+          type: 'address',
+        },
+      ],
+      name: 'isApprovedForAll',
+      outputs: [
+        {
+          internalType: 'bool',
+          name: '',
+          type: 'bool',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'name',
+      outputs: [
+        {
+          internalType: 'string',
+          name: '',
+          type: 'string',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'uint256',
+          name: 'tokenId',
+          type: 'uint256',
+        },
+      ],
+      name: 'ownerOf',
+      outputs: [
+        {
+          internalType: 'address',
+          name: 'owner',
+          type: 'address',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'bytes4',
+          name: 'interfaceId',
+          type: 'bytes4',
+        },
+      ],
+      name: 'supportsInterface',
+      outputs: [
+        {
+          internalType: 'bool',
+          name: '',
+          type: 'bool',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'symbol',
+      outputs: [
+        {
+          internalType: 'string',
+          name: '',
+          type: 'string',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'uint256',
+          name: 'tokenId',
+          type: 'uint256',
+        },
+      ],
+      name: 'tokenURI',
+      outputs: [
+        {
+          internalType: 'string',
+          name: '',
+          type: 'string',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+  ];
+}
