c0afecd572597f632a42f5475e38831f04e061aa
fibonacci998 <fibonacci998@gmail.com>
2024-05-02 15:10:50 +0700

    feat: alter human_readable_topic to text on EvmSignatureMapping table

---
+++ migrations/evm/20240502080623_evm_sign_mapping_alter_human_readable.ts
@@ -0,0 +1,14 @@
+import { Knex } from 'knex';
+import { EvmSignatureMapping } from '../../src/models';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable(EvmSignatureMapping.tableName, (table) => {
+    table.text('human_readable_topic').alter();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable(EvmSignatureMapping.tableName, (table) => {
+    table.string('human_readable_topic', 500).alter();
+  });
+}
