4e89be1885b82dc64b17001223c349cc9e7bb93f
phamphong9981 <phamphong9981@gmail.com>
2024-05-10 15:45:58 +0700

    fix: lint

--- src/models/erc721_activity.ts
+++ src/models/erc721_activity.ts
@@ -1,5 +1,6 @@
 import { Model } from 'objection';
 import BaseModel from './base';
+// eslint-disable-next-line import/no-cycle
 import { Erc721Contract } from './erc721_contract';
 import { EvmEvent } from './evm_event';
 import { EVMSmartContract } from './evm_smart_contract';
