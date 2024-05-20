133d7c66872f54c7c39e3c98521898c2bf28570f
fibonacci998 <fibonacci998@gmail.com>
2024-05-20 16:37:38 +0700

    fix: use raw query to update abi

--- migrations/evm/20240520091605_update_staking_precompile_abi.ts
+++ migrations/evm/20240520091605_update_staking_precompile_abi.ts
@@ -1,24 +1,12 @@
 import { Knex } from 'knex';
-import { EVMContractVerification } from '../../src/models';
 import stakingAbi from '../../src/services/evm/precompile-abis/staking.json' assert { type: 'json' };
 
 export async function up(knex: Knex): Promise<void> {
-  const foundStaking = await EVMContractVerification.query()
-    .findOne({
-      contract_address: '0x0000000000000000000000000000000000000800',
-    })
-    .orderBy('id', 'asc');
-  if (!foundStaking) {
-    throw new Error('Staking contract not found');
-  }
-  await knex.transaction(async (trx) => {
-    await EVMContractVerification.query()
-      .patch({
-        abi: JSON.stringify(stakingAbi),
-      })
-      .where('id', foundStaking.id)
-      .transacting(trx);
-  });
+  await knex.raw(
+    `UPDATE evm_contract_verification SET abi = '${JSON.stringify(
+      stakingAbi
+    )}' where contract_address = '0x0000000000000000000000000000000000000800' and status = 'SUCCESS'`
+  );
 }
 
 export async function down(knex: Knex): Promise<void> {}
