2a67bec28f2e0c6e7f83e85e2e7528d8838f6699
phamphong9981 <phamphong9981@gmail.com>
2024-05-27 16:11:37 +0700

    feat: erc721 reindex

--- src/services/evm/erc721_reindex.ts
+++ src/services/evm/erc721_reindex.ts
@@ -1,3 +1,4 @@
+import knex from 'src/common/utils/db_connection';
 import { PublicClient, getContract } from 'viem';
 import { Erc721Activity, Erc721Contract, Erc721Token } from '../../models';
 
@@ -45,31 +46,66 @@ export class Erc721Reindexer {
    * - reinsert to database
    */
   async reindex(address: `0x${string}`) {
-    await Erc721Activity.query()
-      .delete()
-      .where('erc721_contract_address', address);
-    await Erc721Token.query()
-      .delete()
-      .where('erc721_contract_address', address);
-    await Erc721Contract.query().delete().where('address', address);
+    await knex.transaction(async (trx) => {
+      await Erc721Activity.query()
+        .delete()
+        .where('erc721_contract_address', address)
+        .transacting(trx);
+      await Erc721Token.query()
+        .delete()
+        .where('erc721_contract_address', address)
+        .transacting(trx);
+      await Erc721Contract.query()
+        .delete()
+        .where('address', address)
+        .transacting(trx);
+      const contract = getContract({
+        address,
+        abi: Erc721Contract.ABI,
+        client: this.viemClient,
+      });
+      const contractInfo = await Promise.all([
+        contract.read.name().catch(() => Promise.resolve(undefined)),
+        contract.read.symbol().catch(() => Promise.resolve(undefined)),
+      ]);
+      await Erc721Contract.query()
+        .insert(
+          Erc721Contract.fromJson({
+            evm_smart_contract_id: 0,
+            address,
+            symbol: contractInfo[1],
+            name: contractInfo[0],
+            track: true,
+            last_updated_height: 0,
+          })
+        )
+        .transacting(trx);
+    });
+  }
+
+  async getCurrentTokens(address: `0x${string}`) {
     const contract = getContract({
       address,
       abi: Erc721Contract.ABI,
       client: this.viemClient,
     });
-    const contractInfo = await Promise.all([
-      contract.read.name().catch(() => Promise.resolve(undefined)),
-      contract.read.symbol().catch(() => Promise.resolve(undefined)),
-    ]);
-    console.log(contractInfo);
+    const totalSupply = (await contract.read.totalSupply()) as bigint;
+    const tokensId = (await Promise.all(
+      Array.from(Array(Number(totalSupply)).keys()).map((i) =>
+        contract.read.tokenByIndex([i])
+      )
+    )) as bigint[];
+    const [height, ...owners] = (await Promise.all([
+      this.viemClient.getBlockNumber(),
+      ...tokensId.map((tokenId) => contract.read.ownerOf([tokenId])),
+    ])) as (string | bigint)[];
+    return tokensId.map((tokenId, index) =>
+      Erc721Token.fromJson({
+        token_id: tokenId.toString(),
+        owner: owners[index],
+        erc721_contract_address: address,
+        last_updated_height: Number(height),
+      })
+    );
   }
-
-  // async getCurrentTokens(address: `0x${string}`) {
-  //   const contract = getContract({
-  //     address,
-  //     abi: Erc721Contract.ABI,
-  //     client: this.viemClient,
-  //   });
-
-  // }
 }
