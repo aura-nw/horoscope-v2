9cf479c0a897f542cce671b308d5db1587542cce
phamphong9981 <phamphong9981@gmail.com>
2024-06-28 10:50:45 +0700

    refactor: test

--- test/unit/services/evm/erc20.spec.ts
+++ test/unit/services/evm/erc20.spec.ts
@@ -19,31 +19,51 @@ import {
   EvmEvent,
 } from '../../../../src/models';
 import Erc20Service from '../../../../src/services/evm/erc20.service';
+import { BULL_JOB_NAME } from '../../../../src/services/evm/constant';
+import { SERVICE } from '../../../../src/common/constant';
+import config from '../../../../config.json' assert { type: 'json' };
+import { convertEthAddressToBech32Address } from '../../../../src/services/evm/utils';
 
-@Describe('Test erc20 handler')
-export default class Erc20Test {
+async function cleanQueue(erc20Service: Erc20Service) {
+  await erc20Service
+    .getQueueManager()
+    .getQueue(BULL_JOB_NAME.HANDLE_ERC20_CONTRACT)
+    .drain(true);
+  await erc20Service
+    .getQueueManager()
+    .getQueue(BULL_JOB_NAME.HANDLE_ERC20_ACTIVITY)
+    .drain(true);
+  await erc20Service
+    .getQueueManager()
+    .getQueue(BULL_JOB_NAME.HANDLE_ERC20_BALANCE)
+    .drain(true);
+}
+@Describe('Test erc20 handle balance job')
+export class Erc20Test {
   broker = new ServiceBroker({ logger: false });
 
   erc20Service = this.broker.createService(Erc20Service) as Erc20Service;
 
   evmSmartContract = EVMSmartContract.fromJson({
-    id: 555,
-    address: 'ghghdfgdsgre',
-    creator: 'dfgdfbvxcvxgfds',
+    id: 1,
+    address: '0xE974cC14c93FC6077B0d65F98832B846C5454A0B',
+    creator: '0x5606b4eA93F696Dd82Ca983BAF5723d00729f127',
     created_height: 100,
-    created_hash: 'cvxcvcxv',
+    created_hash:
+      '0x8c46cf6373f2f6e528b56becf0ce6b460b5d90cf9b0325a136b9b3a820e1e489',
     type: EVMSmartContract.TYPES.ERC20,
-    code_hash: 'dfgdfghf',
+    code_hash: '0xdsf3335453454tsgfsdrtserf43645y4h4tAAvfgfgds',
   });
 
   evmSmartContract2 = EVMSmartContract.fromJson({
-    id: 666,
-    address: 'bcvbcvbcv',
-    creator: 'dfgdfbvxcvxgfds',
+    id: 2,
+    address: '0x3CB367e7C920Ff15879Bd4CBd708b8c60eB0f537',
+    creator: '0xa9497CC4F95773A744D408b54dAC724626ee31d2',
     created_height: 100,
-    created_hash: 'xdasfsf',
+    created_hash:
+      '0x5bca9ee42c4c32941c58f2a510dae5ff5c6ed848d9a396a8e9e146a166b3a3fc',
     type: EVMSmartContract.TYPES.PROXY_EIP_1967,
-    code_hash: 'xcsadf',
+    code_hash: '0xdfskjgdsgfgweruwie4535t3tu34tjkewtgjwe',
   });
 
   evmTx = EVMTransaction.fromJson({
@@ -92,11 +112,60 @@ export default class Erc20Test {
     }),
   ];
 
+  account1 = Account.fromJson({
+    id: 1,
+    address: 'aura1w9vxuke5dz6hyza2j932qgmxltnfxwl7gdfgdg',
+    balances: [],
+    spendable_balances: [],
+    type: '',
+    pubkey: '',
+    account_number: 1,
+    sequence: 234,
+    evm_address: '0xDF587daaC47ae7B5586E34bCdb23d0b900b18a6C',
+  });
+
+  account2 = Account.fromJson({
+    id: 2,
+    address: 'aura1w9vxuke5dz6hyza2j932qgmxltnfxwl7qlx4mg',
+    balances: [],
+    spendable_balances: [],
+    type: '',
+    pubkey: '',
+    account_number: 2,
+    sequence: 432,
+    evm_address: '0x71586E5B3468B5720BAa9162A02366Fae6933BfE',
+  });
+
+  account3 = Account.fromJson({
+    id: 3,
+    address: 'aura888vxuke5dz6hyza2j932qgmxltnfxwl7sfsfs',
+    balances: [],
+    spendable_balances: [],
+    type: '',
+    pubkey: '',
+    account_number: 3,
+    sequence: 432,
+    evm_address: '0x7c756Cba10Ff2C65016494E8BA37C12a108572b5',
+  });
+
+  blockCheckpoints = [
+    BlockCheckpoint.fromJson({
+      job_name: BULL_JOB_NAME.HANDLE_ERC20_BALANCE,
+      height: 100,
+    }),
+    BlockCheckpoint.fromJson({
+      job_name: BULL_JOB_NAME.HANDLE_ERC20_ACTIVITY,
+      height: 1000,
+    }),
+  ];
+
   @BeforeAll()
   async initSuite() {
+    await cleanQueue(this.erc20Service);
+    await this.erc20Service.getQueueManager().stopAll();
     await this.broker.start();
     await knex.raw(
-      'TRUNCATE TABLE erc20_contract, account, erc20_activity, evm_smart_contract, evm_event, evm_transaction, account_balance RESTART IDENTITY CASCADE'
+      'TRUNCATE TABLE erc20_contract, account, erc20_activity, evm_smart_contract, evm_event, evm_transaction, account_balance, block_checkpoint RESTART IDENTITY CASCADE'
     );
     await EVMSmartContract.query().insert([
       this.evmSmartContract,
@@ -110,203 +179,161 @@ export default class Erc20Test {
   @BeforeEach()
   async beforeEach() {
     await knex.raw(
-      'TRUNCATE TABLE erc20_activity, account RESTART IDENTITY CASCADE'
+      'TRUNCATE TABLE erc20_activity, block_checkpoint, account RESTART IDENTITY CASCADE'
     );
+    await BlockCheckpoint.query().insert(this.blockCheckpoints);
+    await Account.query().insert([this.account1, this.account2, this.account3]);
   }
 
   @AfterAll()
   async tearDown() {
+    await cleanQueue(this.erc20Service);
     await this.broker.stop();
     jest.resetAllMocks();
     jest.restoreAllMocks();
   }
 
-  /**
-   * @input range blocks
-   * @result erc20Activities in range blocks
-   */
-  @Test('test getErc20Activities')
-  async testGetErc20Activities() {
-    const fromAccount = Account.fromJson({
-      id: 123,
-      address: 'fgsdgfdfgdfgfsdg',
-      balances: [],
-      spendable_balances: [],
-      type: '',
-      pubkey: '',
-      account_number: 1,
-      sequence: 234,
-      evm_address: '0x124537cfdxvfsdfgv',
-    });
-    const toAccount = Account.fromJson({
-      id: 345,
-      address: 'xczfsdfsfsdg',
-      balances: [],
-      spendable_balances: [],
-      type: '',
-      pubkey: '',
-      account_number: 2,
-      sequence: 432,
-      evm_address: '0xghgfhfghfg',
-    });
-    await Account.query().insert([fromAccount, toAccount]);
-    const erc20Activites = [
-      Erc20Activity.fromJson({
-        id: 44444,
-        evm_event_id: this.evmEvent.id,
-        sender: '0xDF587daaC47ae7B5586E34bCdb23d0b900b18a6C',
-        action: 'transfer',
-        erc20_contract_address: this.evmSmartContract.address,
-        amount: '4543',
-        from: fromAccount.evm_address,
-        to: toAccount.evm_address,
-        height: 400,
-        tx_hash:
-          '0x21f905f14a26c5b35e43e1bfbcf8ff395e453d6497a0ce0a0c3cd7814ec0ba03',
-        evm_tx_id: this.evmTx.id,
-      }),
-      Erc20Activity.fromJson({
-        id: 1234,
-        evm_event_id: this.evmEvent.id,
-        sender: '0xa0008c8B4b39Ae646f910B2d2d2F279F92ca6D2b',
-        action: 'transfer',
-        erc20_contract_address: this.evmSmartContract.address,
-        amount: '666666',
-        from: fromAccount.evm_address,
-        to: toAccount.evm_address,
-        height: 401,
-        tx_hash:
-          '0x21f905f14a26c5b35e43e1bfbcf8ff395e453d6497a0ce0a0c3cd7814ec0ba03',
-        evm_tx_id: this.evmTx.id,
-      }),
-      Erc20Activity.fromJson({
-        id: 4444211,
-        evm_event_id: this.evmEvent.id,
-        sender: '0xDF587daaC47ae7B5586E34bCdb23d0b900b18a6C',
-        action: 'transfer',
-        erc20_contract_address: this.evmSmartContract2.address,
-        amount: '4543',
-        from: fromAccount.evm_address,
-        to: toAccount.evm_address,
-        height: 400,
-        tx_hash:
-          '0x21f905f14a26c5b35e43e1bfbcf8ff395e453d6497a0ce0a0c3cd7814ec0ba03',
-        evm_tx_id: this.evmTx.id,
-      }),
-    ];
-    await Erc20Activity.query().insert(erc20Activites);
-    const result = await this.erc20Service.getErc20Activities(0, 10000000);
-    expect(result.length).toEqual(2);
-    expect(result[0]).toMatchObject(erc20Activites[1]);
-    expect(result[0].from_account_id).toEqual(fromAccount.id);
-    expect(result[0].to_account_id).toEqual(toAccount.id);
-    expect(result[1]).toMatchObject(erc20Activites[0]);
-    expect(result[1].from_account_id).toEqual(fromAccount.id);
-    expect(result[1].to_account_id).toEqual(toAccount.id);
-  }
-
-  /**
-   * @description: 3 accounts do transfer with each other
-   * @result updated account balances for each others
-   */
-  @Test('test handleErc20Balance')
+  @Test('handle erc20 balance success')
   async testHandleErc20Balance() {
-    jest.spyOn(BlockCheckpoint, 'getCheckpoint').mockResolvedValue([
-      this.evmSmartContract.created_height - 1,
-      this.evmSmartContract.created_height,
-      BlockCheckpoint.fromJson({
-        job_name: 'dfdsfgsg',
-        height: this.evmSmartContract.created_height - 1,
-      }),
-    ]);
-    const account1 = Account.fromJson({
-      id: 123,
-      address: 'fgsdgfdfgdfgfsdg',
-      balances: [],
-      spendable_balances: [],
-      type: '',
-      pubkey: '',
-      account_number: 1,
-      sequence: 234,
-      evm_address: '0x124537cfdxvfsdfgv',
-    });
-    const account2 = Account.fromJson({
-      id: 345,
-      address: 'xczfsdfsfsdg',
-      balances: [],
-      spendable_balances: [],
-      type: '',
-      pubkey: '',
-      account_number: 2,
-      sequence: 432,
-      evm_address: '0xghgfhfghfg',
-    });
-    const account3 = Account.fromJson({
-      id: 456,
-      address: 'uvvvvvvxxxx',
-      balances: [],
-      spendable_balances: [],
-      type: '',
-      pubkey: '',
-      account_number: 3,
-      sequence: 432,
-      evm_address: '0xabcxyz',
-    });
-    await Account.query().insert([account1, account2, account3]);
     const erc20Activities = [
       {
-        id: 44444,
+        id: 1,
         evm_event_id: this.evmEvent.id,
         sender: '0xDF587daaC47ae7B5586E34bCdb23d0b900b18a6C',
         action: 'transfer',
         erc20_contract_address: this.evmSmartContract.address,
         amount: '4543',
-        from_account_id: account1.id,
-        to_account_id: account2.id,
-        from: account1.evm_address,
-        to: account2.evm_address,
-        height: 400,
+        from: this.account1.evm_address,
+        to: this.account2.evm_address,
+        height: this.blockCheckpoints[0].height + 1,
         tx_hash:
           '0x21f905f14a26c5b35e43e1bfbcf8ff395e453d6497a0ce0a0c3cd7814ec0ba03',
         evm_tx_id: this.evmTx.id,
       },
       {
-        id: 44444,
+        id: 2,
         evm_event_id: this.evmEvent.id,
         sender: '0xDF587daaC47ae7B5586E34bCdb23d0b900b18a6C',
         action: 'transfer',
         erc20_contract_address: this.evmSmartContract.address,
         amount: '2211',
-        from_account_id: account2.id,
-        to_account_id: account3.id,
-        from: account2.evm_address,
-        to: account3.evm_address,
-        height: 400,
+        from: this.account2.evm_address,
+        to: this.account3.evm_address,
+        height: this.blockCheckpoints[0].height + 1,
         tx_hash:
           '0x21f905f14a26c5b35e43e1bfbcf8ff395e453d6497a0ce0a0c3cd7814ec0ba03',
         evm_tx_id: this.evmTx.id,
       },
     ];
-    jest
-      .spyOn(this.erc20Service, 'getErc20Activities')
-      .mockResolvedValue(erc20Activities.map((e) => Erc20Activity.fromJson(e)));
+    await Erc20Activity.query().insert(erc20Activities);
     await this.erc20Service.handleErc20Balance();
     const accountBalances = _.keyBy(
       await AccountBalance.query(),
       (o) => `${o.account_id}_${o.denom}`
     );
     expect(
-      accountBalances[`${account1.id}_${this.evmSmartContract.address}`].amount
+      accountBalances[`${this.account1.id}_${this.evmSmartContract.address}`]
+        .amount
     ).toEqual(`-${BigInt(erc20Activities[0].amount).toString()}`);
     expect(
-      accountBalances[`${account2.id}_${this.evmSmartContract.address}`].amount
+      accountBalances[`${this.account2.id}_${this.evmSmartContract.address}`]
+        .amount
     ).toEqual(
       `${(
         BigInt(erc20Activities[0].amount) - BigInt(erc20Activities[1].amount)
       ).toString()}`
     );
     expect(
-      accountBalances[`${account3.id}_${this.evmSmartContract.address}`].amount
+      accountBalances[`${this.account3.id}_${this.evmSmartContract.address}`]
+        .amount
+    ).toEqual(`${BigInt(erc20Activities[1].amount).toString()}`);
+  }
+
+  @Test('handle erc20 balance with missing account')
+  async testHandleErc20BalanceWithMissingAccount() {
+    const missingAccountId = 5;
+    const missingAccountAddr = '0x5606b4eA93F696Dd82Ca983BAF5723d00729f127';
+    const erc20Activities = [
+      {
+        id: 1,
+        evm_event_id: this.evmEvent.id,
+        sender: '0xDF587daaC47ae7B5586E34bCdb23d0b900b18a6C',
+        action: 'transfer',
+        erc20_contract_address: this.evmSmartContract.address,
+        amount: '4543',
+        from: this.account1.evm_address,
+        to: this.account2.evm_address,
+        height: this.blockCheckpoints[0].height + 1,
+        tx_hash:
+          '0x21f905f14a26c5b35e43e1bfbcf8ff395e453d6497a0ce0a0c3cd7814ec0ba03',
+        evm_tx_id: this.evmTx.id,
+      },
+      {
+        id: 2,
+        evm_event_id: this.evmEvent.id,
+        sender: '0xDF587daaC47ae7B5586E34bCdb23d0b900b18a6C',
+        action: 'transfer',
+        erc20_contract_address: this.evmSmartContract.address,
+        amount: '2211',
+        from: missingAccountAddr,
+        to: this.account3.evm_address,
+        height: this.blockCheckpoints[0].height + 1,
+        tx_hash:
+          '0x21f905f14a26c5b35e43e1bfbcf8ff395e453d6497a0ce0a0c3cd7814ec0ba03',
+        evm_tx_id: this.evmTx.id,
+      },
+    ];
+    await Erc20Activity.query().insert(erc20Activities);
+    const mockCall = jest
+      .spyOn(this.erc20Service.broker, 'call')
+      .mockImplementation(async () =>
+        Promise.resolve(
+          Account.query().insert(
+            Account.fromJson({
+              id: missingAccountId,
+              address: 'aura12crtf65n76tdmqk2nqa674er6qrjnuf8h89p22',
+              balances: [],
+              spendable_balances: [],
+              type: '',
+              pubkey: '',
+              account_number: 4,
+              sequence: 2,
+              evm_address: missingAccountAddr,
+            })
+          )
+        )
+      );
+    await this.erc20Service.handleErc20Balance();
+    const accountBalances = _.keyBy(
+      await AccountBalance.query(),
+      (o) => `${o.account_id}_${o.denom}`
+    );
+    expect(mockCall).toHaveBeenCalledWith(
+      SERVICE.V1.HandleAddressService.CrawlNewAccountApi.path,
+      {
+        addresses: [
+          convertEthAddressToBech32Address(
+            config.networkPrefixAddress,
+            missingAccountAddr
+          ),
+        ],
+      }
+    );
+    expect(
+      accountBalances[`${this.account1.id}_${this.evmSmartContract.address}`]
+        .amount
+    ).toEqual(`-${BigInt(erc20Activities[0].amount).toString()}`);
+    expect(
+      accountBalances[`${this.account2.id}_${this.evmSmartContract.address}`]
+        .amount
+    ).toEqual(`${BigInt(erc20Activities[0].amount).toString()}`);
+    expect(
+      accountBalances[`${missingAccountId}_${this.evmSmartContract.address}`]
+        .amount
+    ).toEqual(`-${BigInt(erc20Activities[1].amount).toString()}`);
+    expect(
+      accountBalances[`${this.account3.id}_${this.evmSmartContract.address}`]
+        .amount
     ).toEqual(`${BigInt(erc20Activities[1].amount).toString()}`);
   }
 }
--- test/unit/services/evm/evm_crawl_internal_tx.spec.ts
+++ test/unit/services/evm/evm_crawl_internal_tx.spec.ts
@@ -10,12 +10,7 @@ export default class TestEvmCrawlInternalTx {
     EvmCrawlInternalTxService
   ) as EvmCrawlInternalTxService;
 
-  /**
-   * @description test buildEvmInternalTransaction function
-   * @input result from eth_debugTracecall
-   * @result build evm internal transaction
-   */
-  @Test('Test buildEvmInternalTransaction')
+  @Test('Test buildEvmInternalTransaction normal case')
   async testBuildEvmInternalTransaction() {
     const result = {
       calls: [
@@ -72,6 +67,151 @@ export default class TestEvmCrawlInternalTx {
       input: result.calls[1].input,
       gas: parseInt(result.calls[1].gas, 16),
       gas_used: parseInt(result.calls[1].gasUsed, 16),
+    });
+  }
+
+  @Test('Test buildEvmInternalTransaction complicated call')
+  async testBuildEvmInternalTransactionComplicatedCall() {
+    const result = {
+      calls: [
+        {
+          from: '0x98605ae21dd3be686337a6d7a8f156d0d8baee92',
+          gas: '0x67342',
+          gasUsed: '0x5d26',
+          input: '0xd0e30db0',
+          output: '0x',
+          to: '0xe974cc14c93fc6077b0d65f98832b846c5454a0b',
+          type: 'CALL',
+          value: '0x8ac7230489e80000',
+        },
+        {
+          from: '0x98605ae21dd3be686337a6d7a8f156d0d8baee92',
+          gas: '0x6166e',
+          gasUsed: '0x1ec2',
+          input:
+            '0x23b872dd00000000000000000000000098605ae21dd3be686337a6d7a8f156d0d8baee92000000000000000000000000c7663c6a454fc9971c93235a170c8997e8c5e6610000000000000000000000000000000000000000000000008ac7230489e80000',
+          output:
+            '0x0000000000000000000000000000000000000000000000000000000000000001',
+          to: '0xe974cc14c93fc6077b0d65f98832b846c5454a0b',
+          type: 'CALL',
+          value: '0x0',
+        },
+        {
+          from: '0x98605ae21dd3be686337a6d7a8f156d0d8baee92',
+          gas: '0x5f6af',
+          gasUsed: '0x1be',
+          input:
+            '0x70a08231000000000000000000000000c7663c6a454fc9971c93235a170c8997e8c5e661',
+          output:
+            '0x000000000000000000000000000000000000000000000001d7d843dc3b480000',
+          to: '0xe974cc14c93fc6077b0d65f98832b846c5454a0b',
+          type: 'STATICCALL',
+        },
+        {
+          from: '0x98605ae21dd3be686337a6d7a8f156d0d8baee92',
+          gas: '0x5f372',
+          gasUsed: '0xa43',
+          input:
+            '0xdd62ed3e000000000000000000000000c7663c6a454fc9971c93235a170c8997e8c5e661000000000000000000000000d1fc2d4f9984b6214d0a3778cbacbabfa5e84224',
+          output:
+            '0x0000000000000000000000000000000000000000000000008ac7230489e80000',
+          to: '0xe974cc14c93fc6077b0d65f98832b846c5454a0b',
+          type: 'STATICCALL',
+        },
+        {
+          calls: [
+            {
+              from: '0x56e88b1731d55703c1f80391602297715886fa6a',
+              gas: '0x5bb52',
+              gasUsed: '0xab0',
+              input:
+                '0x37436c98000000000000000000000000c7663c6a454fc9971c93235a170c8997e8c5e66100000000000000000000000098605ae21dd3be686337a6d7a8f156d0d8baee92',
+              output:
+                '0x0000000000000000000000000000000000000000000000000000000000000001',
+              to: '0xb28d39287ec900e0987f5dd755651de4256d0f5b',
+              type: 'STATICCALL',
+            },
+          ],
+          from: '0x98605ae21dd3be686337a6d7a8f156d0d8baee92',
+          gas: '0x5de59',
+          gasUsed: '0x1771',
+          input:
+            '0x72d27692000000000000000000000000c7663c6a454fc9971c93235a170c8997e8c5e661',
+          output:
+            '0x0000000000000000000000000000000000000000000000000000000000000001',
+          to: '0x56e88b1731d55703c1f80391602297715886fa6a',
+          type: 'STATICCALL',
+        },
+      ],
+      from: '0xc7663c6a454fc9971c93235a170c8997e8c5e661',
+      gas: '0x74994',
+      gasUsed: '0x22f96',
+      input:
+        '0xa85d83ad000000000000000000000000c211c2cf383a38933f8352cbdd326b51b94574e6000000000000000000000000000000000000000000000000000000000000000e00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000008ac7230489e800000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
+      output:
+        '0x000000000000000000000000000000000000000000000000000000000000000c',
+      to: '0x98605ae21dd3be686337a6d7a8f156d0d8baee92',
+      type: 'CALL',
+      value: '0x8ac7230489e80000',
+    };
+    const internalTxs =
+      this.evmCrawlInternalTxService.buildEvmInternalTransaction(result, 1);
+    expect(internalTxs.length).toEqual(6);
+    expect(internalTxs[0]).toMatchObject({
+      evm_tx_id: '1',
+      type: result.calls[0].type,
+      from: result.calls[0].from,
+      to: result.calls[0].to,
+      value: parseInt(result.calls[0].value || '0', 16),
+      input: result.calls[0].input,
+      gas: parseInt(result.calls[0].gas, 16),
+      gas_used: parseInt(result.calls[0].gasUsed, 16),
+    });
+    expect(internalTxs[1]).toMatchObject({
+      type: result.calls[1].type,
+      from: result.calls[1].from,
+      to: result.calls[1].to,
+      value: parseInt(result.calls[1].value || '0', 16),
+      input: result.calls[1].input,
+      gas: parseInt(result.calls[1].gas, 16),
+      gas_used: parseInt(result.calls[1].gasUsed, 16),
+    });
+    expect(internalTxs[2]).toMatchObject({
+      type: result.calls[2].type,
+      from: result.calls[2].from,
+      to: result.calls[2].to,
+      value: parseInt(result.calls[2].value || '0', 16),
+      input: result.calls[2].input,
+      gas: parseInt(result.calls[2].gas, 16),
+      gas_used: parseInt(result.calls[2].gasUsed, 16),
+    });
+    expect(internalTxs[3]).toMatchObject({
+      type: result.calls[3].type,
+      from: result.calls[3].from,
+      to: result.calls[3].to,
+      value: parseInt(result.calls[3].value || '0', 16),
+      input: result.calls[3].input,
+      gas: parseInt(result.calls[3].gas, 16),
+      gas_used: parseInt(result.calls[3].gasUsed, 16),
+    });
+    expect(internalTxs[4]).toMatchObject({
+      type: result.calls[4].type,
+      from: result.calls[4].from,
+      to: result.calls[4].to,
+      value: parseInt(result.calls[4].value || '0', 16),
+      input: result.calls[4].input,
+      gas: parseInt(result.calls[4].gas, 16),
+      gas_used: parseInt(result.calls[4].gasUsed, 16),
+    });
+    const nestedCall = result.calls[4].calls || [];
+    expect(nestedCall?.length).toEqual(1);
+    expect(internalTxs[5]).toMatchObject({
+      type: nestedCall[0].type,
+      from: nestedCall[0].from,
+      to: nestedCall[0].to,
+      input: nestedCall[0].input,
+      gas: parseInt(nestedCall[0].gas, 16),
+      gas_used: parseInt(nestedCall[0].gasUsed, 16),
     });
   }
 }
