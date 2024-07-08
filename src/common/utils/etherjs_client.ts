6d6b5a45bb91c20cd20b40271fc5f2a26f667bfb
Phạm Thành Phong <49814372+phamphong9981@users.noreply.github.com>
2024-07-08 17:16:04 +0700

    Refactor/repalce etherjs by viem main (#838) TG-218 #waiting
    * refactor: replace etherjs by viem
    
    * refactor: replace etherjs by viem
    
    * refactor: replace etherjs by viem
    
    * fix: test
    
    * refactor: review
    
    * refactor: review
    
    * refactor: review
    
    * refactor: update viem to 2.14.0
    
    * refactor: review
    
    * refactor: review
    
    * refactor: merge

--- src/common/utils/etherjs_client.ts
+++ src/common/utils/etherjs_client.ts
@@ -1,32 +1,19 @@
-import { ethers } from 'ethers';
 import { PublicClient, createPublicClient, http } from 'viem';
-import networks from '../../../network.json' assert { type: 'json' };
 import config from '../../../config.json' assert { type: 'json' };
+import '../../../fetch-polyfill.js';
+import networks from '../../../network.json' assert { type: 'json' };
 
-export default class EtherJsClient {
-  public etherJsClient: ethers.AbstractProvider;
+let viemClient!: PublicClient;
 
-  constructor() {
-    const selectedChain = networks.find(
-      (network) => network.chainId === config.chainId
-    );
-    if (selectedChain?.EVMJSONRPC) {
-      this.etherJsClient = ethers.getDefaultProvider(
-        selectedChain.EVMJSONRPC[0]
-      );
-    } else {
-      throw new Error(`EVMJSONRPC not found with chainId: ${config.chainId}`);
-    }
-  }
-
-  public static getViemClient(): PublicClient {
+export function getViemClient(): PublicClient {
+  if (!viemClient) {
     const selectedChain = networks.find(
       (network) => network.chainId === config.chainId
     );
     if (!selectedChain?.EVMJSONRPC) {
       throw new Error(`EVMJSONRPC not found with chainId: ${config.chainId}`);
     }
-    return createPublicClient({
+    viemClient = createPublicClient({
       batch: {
         multicall: {
           batchSize: config.viemConfig.multicall.batchSize,
@@ -41,4 +28,5 @@ export default class EtherJsClient {
       }),
     });
   }
+  return viemClient;
 }
