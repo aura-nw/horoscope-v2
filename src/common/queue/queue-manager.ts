01336915c11a954f58afa5d286dbeec396b22848
phamphong9981 <phamphong9981@gmail.com>
2024-07-12 11:19:32 +0700

    feat: pino log discord & handle throw

--- src/common/queue/queue-manager.ts
+++ src/common/queue/queue-manager.ts
@@ -60,8 +60,12 @@ export default class QueueManager {
   ) {
     // bind the owner, so 'this' can be accessed in the handler function
     const f = async (payload: object) => {
-      const func = this._handlerOwner ? fn.bind(this._handlerOwner) : fn;
-      await func(payload);
+      try {
+        const func = this._handlerOwner ? fn.bind(this._handlerOwner) : fn;
+        await func(payload);
+      } catch (error: any) {
+        this._handlerOwner.logger.error(error.message);
+      }
     };
     // register the handler
     this._queueProvider.registerQueueHandler(qOpt, f);
