231644154870003203feb70082eb96e95a8b994b
phamphong9981 <phamphong9981@gmail.com>
2024-09-09 10:09:00 +0700

    refactor: index

--- migrations/evm/20240905021812_m_view_erc721_holder_statistic.ts
+++ migrations/evm/20240905021812_m_view_erc721_holder_statistic.ts
@@ -6,7 +6,13 @@ export async function up(knex: Knex): Promise<void> {
   CREATE MATERIALIZED VIEW m_view_erc721_holder_statistic AS
     select count(*), erc721_token.owner, erc721_token.erc721_contract_address
     from erc721_token 
-    group by erc721_token.owner, erc721_token.erc721_contract_address
+    group by erc721_token.owner, erc721_token.erc721_contract_address;
+  CREATE INDEX m_view_erc721_holder_statistic_owner_index
+    ON m_view_erc721_holder_statistic (owner);
+  CREATE INDEX m_view_erc721_holder_statistic_erc721_contract_address_index
+    ON m_view_erc721_holder_statistic (erc721_contract_address);
+  CREATE INDEX m_view_erc721_holder_statistic_count_index
+    ON m_view_erc721_holder_statistic (count);
 `);
 }
 
