9c92566173f940ac24bc348e97d166e86ffd14fb
fibonacci998 <fibonacci998@gmail.com>
2024-10-03 14:04:55 +0700

    fix: set statement_timeout to run migration

--- migrations/evm/20241003034314_create_from_to_account_id_in_evm_tx.ts
+++ migrations/evm/20241003034314_create_from_to_account_id_in_evm_tx.ts
@@ -1,6 +1,7 @@
 import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
+  await knex.raw(`set statement_timeout to 0`);
   await knex.schema.alterTable('evm_transaction', (table) => {
     table.integer('from_account_id').index();
     table.integer('to_account_id').index();
